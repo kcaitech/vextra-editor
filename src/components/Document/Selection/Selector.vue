e15c3a3781fb6aafaea96058fb195e258316e931
Zrx <georgezrx@163.com>
2023-06-21 11:41:37 +0800

    remove selection shape

--- src/components/Document/Selection/Selector.vue
+++ src/components/Document/Selection/Selector.vue
@@ -21,6 +21,7 @@ const selectedShapes: Shape[] = [];
 function select() {
     const pageMatirx = props.context.workspace.matrix;
     const page = props.context.selection.selectedPage;
+    const selection = props.context.selection;
     if (page) {
         const { top, left, width, height } = props.selectorFrame;
         const p1: XY = pageMatirx.inverseCoord(left, top); // lt
@@ -28,45 +29,67 @@ function select() {
         const p3: XY = pageMatirx.inverseCoord(left + width, top + height); // rb
         const p4: XY = pageMatirx.inverseCoord(left, top + height); //lb
         const ps: [XY, XY, XY, XY, XY] = [p1, p2, p3, p4, p1]; // 5个点方便闭合循环
-        deep(page.childs, ps);
-        if (selectedShapes.length !== props.context.selection.selectedShapes.length) {
-            props.context.selection.rangeSelectShape(selectedShapes);
+        remove(selectedShapes, ps);
+        finder(page.childs, ps);
+        if (selectedShapes.length !== selection.selectedShapes.length) {
+            selection.rangeSelectShape(selectedShapes);
         }
     }
 
-    function deep(childs: Shape[], Points: [XY, XY, XY, XY, XY]) {
-        let ids = 0;
-        while (ids < childs.length) {
-            if (selectedShapes.find(i => i.id === childs[ids].id)) {
-                ids++;
-                continue;
-            }
-            if (childs[ids].isLocked || !childs[ids].isVisible) {
-                ids++;
-                continue;
-            }
-            const m = childs[ids].matrix2Page();
-            const { width: w, height: h } = childs[ids].frame;
-            const ps: XY[] = [
-                { x: 0, y: 0 },
-                { x: w, y: 0 },
-                { x: w, y: h },
-                { x: 0, y: h },
-                { x: 0, y: 0 },
-            ].map(p => m.computeCoord(p.x, p.y));
-            if (isTarget(Points, ps)) {
-                selectedShapes.push(childs[ids]);
-            }
+}
+// 加入
+function finder(childs: Shape[], Points: [XY, XY, XY, XY, XY]) {
+    let ids = 0;
+    while (ids < childs.length) {
+        if (selectedShapes.find(i => i.id === childs[ids].id)) {
             ids++;
+            continue;
         }
+        if (childs[ids].isLocked || !childs[ids].isVisible) {
+            ids++;
+            continue;
+        }
+        const m = childs[ids].matrix2Page();
+        const { width: w, height: h } = childs[ids].frame;
+        const ps: XY[] = [
+            { x: 0, y: 0 },
+            { x: w, y: 0 },
+            { x: w, y: h },
+            { x: 0, y: h },
+            { x: 0, y: 0 },
+        ].map(p => m.computeCoord(p.x, p.y));
+        if (isTarget(Points, ps)) {
+            selectedShapes.push(childs[ids]);
+        }
+        ids++;
     }
 }
+// 剔除
+function remove(childs: Shape[], Points: [XY, XY, XY, XY, XY]) {
+    let ids = 0;
+    while (ids < childs.length) {
+        const m = childs[ids].matrix2Page();
+        const { width: w, height: h } = childs[ids].frame;
+        const ps: XY[] = [
+            { x: 0, y: 0 },
+            { x: w, y: 0 },
+            { x: w, y: h },
+            { x: 0, y: h },
+            { x: 0, y: 0 },
+        ].map(p => m.computeCoord(p.x, p.y));
+        if (!isTarget(Points, ps)) {
+            selectedShapes.splice(ids, 1);
+        }
+        ids++;
+    }
+}
+
 function reset(t?: number) {
     if (t === WorkSpace.SELECTING) {
         selectedShapes.length = 0;
     }
 }
-watchEffect(select);
+// hooks
 onMounted(() => {
     props.context.workspace.watch(reset);
     selectedShapes.length = 0;
@@ -74,6 +97,7 @@ onMounted(() => {
 onUnmounted(() => {
     props.context.workspace.unwatch(reset);
 })
+watchEffect(select);
 </script>
 <template>
     <div class="selector"
