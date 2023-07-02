1e749fa53aa4e0f393abafebda21eb51e7ae0d3b
zrx <georgezrx@163.com>
2023-07-03 00:12:51 +0800

    处理容器渲染问题

--- src/render/artboard.ts
+++ src/render/artboard.ts
@@ -4,7 +4,7 @@ import { Color } from "@kcdesign/data";
 import { renderGroupChilds as gR } from "@/render/group";
 
 const defaultColor = new Color(1, 255, 255, 255);
-
+// artboard单独一个svg节点，需要设置overflow
 export function render(h: Function, shape: Artboard, comsMap: Map<ShapeType, any>, reflush?: number) {
     if (!shape.isVisible) return;
     const ab_props: any = {
@@ -20,7 +20,6 @@ export function render(h: Function, shape: Artboard, comsMap: Map<ShapeType, any
     const frame = shape.frame;
     ab_props.x = frame.x, ab_props.y = frame.y, ab_props.width = frame.width, ab_props.height = frame.height;
     ab_props.viewBox = `0 0 ${frame.width} ${frame.height}`;
-
     // background
     if (shape.hasBackgroundColor) {
         const color = shape.backgroundColor || defaultColor;
@@ -30,7 +29,34 @@ export function render(h: Function, shape: Artboard, comsMap: Map<ShapeType, any
         }))
     }
     childs.push(...gR(h, shape, comsMap));
-
-    // artboard单独一个svg节点，需要设置overflow
-    return h('svg', ab_props, childs);
+    // <svg>
+    //  <svg></svg>
+    // </svg>
+    // 当一个<svg />里面嵌套一个<svg />(如上结构)时，会无法给里面的<svg />设置旋转属性，这里利用foreignObject隔离(如下结构)这种嵌套
+    // <svg>
+    //  <foreignObject>
+    //      <div>
+    //          <svg></svg>   <----真实展示的元素
+    //      </div>
+    //  </foreignObject>
+    // </svg>
+    if (shape.rotation) {
+        const foreign_object_props = {
+            x: frame.x,
+            y: frame.y,
+            width: frame.width,
+            height: frame.height,
+            overflow: "visible",
+        }
+        ab_props.x = 0, ab_props.y = 0;
+        ab_props.style = `transform-origin: center center; transform: rotate(${shape.rotation}deg)`;
+        const div_props = {
+            width: frame.width,
+            height: frame.height,
+            overflow: "visible",
+        }
+        return h('foreignObject', foreign_object_props, h('div', div_props, h('svg', ab_props, childs)));
+    } else {
+        return h('svg', ab_props, childs);
+    }
 }
