da6d225cec61ebf0bdedc11b950281766ac8b0e9
zrx <georgezrx@163.com>
2023-07-02 23:28:50 +0800

    容器旋转

--- src/render/artboard.ts
+++ src/render/artboard.ts
@@ -3,12 +3,24 @@ import { ShapeType } from "@kcdesign/data";
 import { Color } from "@kcdesign/data";
 import { renderGroupChilds as gR } from "@/render/group";
 
-const defaultColor = new Color(1, 255, 255, 255)
+const defaultColor = new Color(1, 255, 255, 255);
 
 export function render(h: Function, shape: Artboard, comsMap: Map<ShapeType, any>, reflush?: number) {
     if (!shape.isVisible) return;
+    const ab_props: any = {
+        version: "1.1",
+        xmlns: "http://www.w3.org/2000/svg",
+        "xmlns:xlink": "http://www.w3.org/1999/xlink",
+        "xmlns:xhtml": "http://www.w3.org/1999/xhtml",
+        preserveAspectRatio: "xMinYMin meet",
+        reflush,
+        overflow: "hidden",
+    }
     const childs = [];
     const frame = shape.frame;
+    ab_props.x = frame.x, ab_props.y = frame.y, ab_props.width = frame.width, ab_props.height = frame.height;
+    ab_props.viewBox = `0 0 ${frame.width} ${frame.height}`;
+
     // background
     if (shape.hasBackgroundColor) {
         const color = shape.backgroundColor || defaultColor;
@@ -18,19 +30,7 @@ export function render(h: Function, shape: Artboard, comsMap: Map<ShapeType, any
         }))
     }
     childs.push(...gR(h, shape, comsMap));
+
     // artboard单独一个svg节点，需要设置overflow
-    return h('svg', {
-        version: "1.1",
-        xmlns: "http://www.w3.org/2000/svg",
-        "xmlns:xlink": "http://www.w3.org/1999/xlink",
-        "xmlns:xhtml": "http://www.w3.org/1999/xhtml",
-        preserveAspectRatio: "xMinYMin meet",
-        width: frame.width,
-        height: frame.height,
-        viewBox: "0 0 " + frame.width + " " + frame.height,
-        x: frame.x,
-        y: frame.y,
-        overflow: "hidden",
-        reflush
-    }, childs);
+    return h('svg', ab_props, childs);
 }
