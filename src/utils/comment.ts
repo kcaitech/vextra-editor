99baa47d8c6d86e56077bad5534af95ef6599509
Kc_tjs <3288771283@qq.com>
2023-07-03 16:02:50 +0800

    update

--- src/utils/comment.ts
+++ src/utils/comment.ts
@@ -10,14 +10,14 @@ export function searchCommentShape(context: Context, position: PageXY) {
 }
 export function finder(scout: Scout, g: Shape[], position: PageXY, selected: Shape, init?: Shape[]): Shape[] {
     const result = init || [];
-    for (let i = g.length - 1; i > -1; i--) { // 从最上层开始往下找(z-index：大 -> 小)
-        if (canBeTarget(g[i])) { // 只要是!isVisible，force与否都不可以选中
+    for (let i = g.length - 1; i > -1; i--) { 
+        if (canBeTarget(g[i])) {
             const item = g[i];
-            if ([ShapeType.Group, ShapeType.FlattenShape, ShapeType.Artboard].includes(item.type)) { // 如果是容器或者编组
+            if ([ShapeType.Group, ShapeType.FlattenShape, ShapeType.Artboard].includes(item.type)) {
                 const isItemIsTarget = isTarget(scout, item, position);
                 if (!isItemIsTarget) continue; // 如果整个容器和编组都不是目标元素，则不需要向下遍历
                 const c = item.childs as Shape[];
-                if (item.type === ShapeType.Artboard) { // 如果是容器，有子元素时不可以被hover    
+                if (item.type === ShapeType.Artboard) {
                     if (c.length) {
                         result.push(...finder(scout, c, position, selected, result));
                         if (result.length) {
@@ -30,7 +30,7 @@ export function finder(scout: Scout, g: Shape[], position: PageXY, selected: Sha
                         result.push(item);
                         return result;
                     }
-                } else if ([ShapeType.Group, ShapeType.FlattenShape].includes(item.type)) { // 如果是编组，不用向下走了，让子元素往上走
+                } else if ([ShapeType.Group, ShapeType.FlattenShape].includes(item.type)) {
                     const g = forGroupHover(scout, item.childs, position, selected, true);
                     if (g) {
                         result.push(g);
