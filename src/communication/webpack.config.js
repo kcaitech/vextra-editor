ab7b43b53538450bd9ffb62c76ed5a82fc35067d
陈柱锋 <2414802324@qq.com>
2023-07-13 17:32:09 +0800

    communication实时通信模块

--- src/communication/webpack.config.js
+++ src/communication/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     },
     output: {
         filename: "worker.js",
-        // path: path.resolve(__dirname, "dist"),
-        path: path.resolve(__dirname, "../../public"),
+        path: path.resolve(__dirname, "../../dist"),
+        // path: path.resolve(__dirname, "../../public"),
     },
 };
