461c2845ebeddf38c9366d29d4a6b8622d778440
Kc_tjs <3288771283@qq.com>
2023-08-10 09:28:07 +0800

    用户进入文档获取选区头像

--- vue.config.js
+++ vue.config.js
@@ -112,8 +112,7 @@ var configureWebpack = (config) => {
 
 var exports = defineConfig({
     transpileDependencies: true,
-    // publicPath: './',
-    publicPath: '/tjs',
+    publicPath: './',
     configureWebpack,
 
     pluginOptions: {
