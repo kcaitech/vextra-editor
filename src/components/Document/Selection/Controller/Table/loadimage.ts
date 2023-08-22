b18ac985b5846a02ccff89d8c573c35e748a5b5f
Zrx <georgezrx@163.com>
2023-08-22 17:40:55 +0800

    加入胖虎

--- src/components/Document/Selection/Controller/Table/loadimage.ts
+++ src/components/Document/Selection/Controller/Table/loadimage.ts
@@ -1,6 +1,16 @@
 import { FilePicker } from "@/components/common/filepicker";
 import { onUnmounted } from "vue";
-
+function base64ToDataUrl(format: string, base64: string) {
+    format = format.toLocaleLowerCase();
+    const de_fileheader = new Map([
+        ['svg', `data:image/svg+xml;base64,${base64}`,],
+        ['gif', `data:image/gif;base64,${base64}`],
+        ['jpeg', `data:image/jpeg;base64,${base64}`],
+        ['jpg', `data:image/jpeg;base64,${base64}`],
+        ['png', `data:image/png;base64,${base64}`]
+    ])
+    return de_fileheader.get(format) || '';
+}
 export function loadImage(name: string, buffer: ArrayBuffer) {
     const uInt8Array = new Uint8Array(buffer);
     let i = uInt8Array.length;
@@ -9,25 +19,11 @@ export function loadImage(name: string, buffer: ArrayBuffer) {
         binaryString[i] = String.fromCharCode(uInt8Array[i]);
     }
     const data = binaryString.join('');
-
-    const base64 = window.btoa(data);
-
-    let url = '';
+    const base64 = btoa(data);
     const ext = name.substring(name.lastIndexOf('.') + 1);
-    if (ext == "png") {
-        url = "data:image/png;base64," + base64;
-    }
-    else if (ext == "gif") {
-        url = "data:image/gif;base64," + base64;
-    }
-    else {
-        console.log("imageExt", ext);
-    }
-
+    const url = base64ToDataUrl(ext, base64);
     return { buff: uInt8Array, base64: url };
 }
-
-
 const accept = 'image/png, image/jpeg, image/gif, image/svg+xml, image/icns';
 export function useImagePicker() {
     let filePicker: FilePicker | undefined;
