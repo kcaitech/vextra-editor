2dd48bff523554f46768dfb3498ef8384df503b8
ice <icezobo12@gmail.com>
2023-08-30 18:11:22 +0800

    提交代码

--- src/components/TeamProject/TeamMember.vue
+++ src/components/TeamProject/TeamMember.vue
@@ -2,7 +2,7 @@
     <div v-if="!noNetwork" class="container">
         <div class="hearder-container">
             <div class="title" v-for="(item, index) in  titles " :key="index">
-                <div>{{ item }}
+                <div class="content">{{ item }}
                     <div v-if="index === 1" class="shrink" @click="fold = !fold">
                         <svg-icon icon-class="down"
                             :style="{ transform: fold ? 'rotate(-180deg)' : 'rotate(0deg)' }"></svg-icon>
@@ -24,14 +24,62 @@
             <div class="member-item"
                 v-for=" { user: { nickname, id }, perm_type }  in  searchvalue === '' ? ListData : SearchList " :key="id">
                 <div class="member-name">{{ nickname }}</div>
-                <div class="member-jurisdiction">{{ membertype(perm_type) }}</div>
+                <div class="member-jurisdiction">
+                    <div v-if="usertype() === 3" class="member-jurisdiction-container">
+                        {{ membertype(perm_type) }}
+                        <div v-if="perm_type !== 3" class="shrink" @click="folds = !folds, userid = id">
+                            <svg-icon icon-class="down"
+                                :style="{ transform: folds && userid === id ? 'rotate(-180deg)' : 'rotate(0deg)' }"></svg-icon>
+                        </div>
+                        <transition name="el-zoom-in-top">
+                            <ul class="filterlist" v-if="userid === id && folds" ref="menu">
+                                <li class="item" v-for="(item, index) in  typeitems " :key="index"
+                                    @click.stop="filterEvent(index)">
+                                    <div class="choose" :style="{ visibility: index == fontName ? 'visible' : 'hidden' }">
+                                    </div>
+                                    {{ item }}
+                                </li>
+                            </ul>
+                        </transition>
+                    </div>
+                    <div v-if="usertype() === 2" class="member-jurisdiction-container">
+                        {{ membertype(perm_type) }}
+                        <div v-if="perm_type !== 3 && 2" class="shrink" @click="folds = !folds, userid = id">
+                            <svg-icon icon-class="down"
+                                :style="{ transform: folds && userid === id ? 'rotate(-180deg)' : 'rotate(0deg)' }"></svg-icon>
+                        </div>
+                        <transition name="el-zoom-in-top">
+                            <ul class="filterlist" v-if="userid === id && folds" ref="menu">
+                                <li class="item" v-for="(item, index) in  filteritems " :key="index"
+                                    @click.stop="filterEvent(index)">
+                                    {{ item }}
+                                </li>
+                            </ul>
+                        </transition>
+                    </div>
+                    <div v-if="usertype() === 0 || usertype() === 1" class="member-jurisdiction-container">
+                        {{ membertype(perm_type) }}
+                        <div v-if="userID === id" class="shrink" @click="folds = !folds, userid = id">
+                            <svg-icon icon-class="down"
+                                :style="{ transform: folds && userid === id ? 'rotate(-180deg)' : 'rotate(0deg)' }"></svg-icon>
+                        </div>
+                        <transition name="el-zoom-in-top">
+                            <ul class="filterlist" v-if="userid === id && folds" ref="menu">
+                                <li class="item" v-for="(item, index) in  filteritems " :key="index"
+                                    @click.stop="filterEvent(index)">
+                                    {{ item }}
+                                </li>
+                            </ul>
+                        </transition>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
     <NetworkError v-else @refresh-doc="GetteamMember"></NetworkError>
 </template>
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref, inject, Ref, watch, computed } from 'vue';
+import { onMounted, onUnmounted, ref, inject, Ref, watch, computed, nextTick } from 'vue';
 import NetworkError from '@/components/NetworkError.vue'
 import * as user_api from '@/apis/users'
 import { ElMessage } from 'element-plus'
@@ -44,17 +92,32 @@ interface Props {
 const props = withDefaults(defineProps<Props>(), {
     searchvalue: ''
 })
+
+const userID = ref(localStorage.getItem('userId'))
+const userid = ref()
 const { t } = useI18n()
 const titles = ['姓名', '团队权限']
 const filteritems = ['仅阅读', '可编辑', '管理员', '创建者', '全部']
+const typeitems = ['管理员', '可编辑', '仅阅读', '转移创建者', '移出团队']
+
 const noNetwork = ref(false)
 const teammemberdata = ref<any[]>([])
 const fold = ref(false)
+const folds = ref(false)
 const fontName = ref(4)
 const menu = ref<HTMLElement>()
 
 const { teamID } = inject('shareData') as {
     teamID: Ref<string>;
+}
+
+const usertype = () => {
+    const text = teammemberdata.value.find((item) => item.user.id === userID.value)
+    if (text) {
+        return text.perm_type
+    }
+
+
 }
 
 const GetteamMember = async () => {
@@ -113,6 +176,7 @@ const ListData = computed(() => {
 const filterEvent = (index: number = 4) => {
     fontName.value = index
     fold.value = false
+    folds.value = false
 }
 
 watch(teamID, () => {
@@ -141,6 +205,117 @@ onUnmounted(() => {
             display: flex;
             align-items: center;
 
+            .content {
+                display: flex;
+                justify-content: center;
+
+                .shrink {
+                    width: 16px;
+                    height: 16px;
+                    float: right;
+                    line-height: 25px;
+
+                    >svg {
+                        transition: 0.5s;
+                        width: 100%;
+                        height: 100%;
+                        margin-left: 4px;
+                    }
+                }
+
+                .filterlist {
+                    position: absolute;
+                    list-style-type: none;
+                    font-size: 14px;
+                    font-weight: 500;
+                    min-width: 72px;
+                    margin: 0;
+                    padding: 0 8px;
+                    margin-top: 24px;
+                    border-radius: 4px;
+                    background-color: white;
+                    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+                    z-index: 3;
+
+                    .choose {
+                        box-sizing: border-box;
+                        width: 10px;
+                        height: 6px;
+                        margin-right: 4px;
+                        margin-left: 2px;
+                        border-width: 0 0 1px 1px;
+                        border-style: solid;
+                        border-color: rgb(0, 0, 0, .75);
+                        transform: rotate(-45deg) translateY(-30%);
+                    }
+
+                    .item {
+                        cursor: pointer;
+                        display: flex;
+                        align-items: center;
+                        line-height: 32px;
+                    }
+                }
+            }
+
+
+
+
+        }
+    }
+
+    .member-item {
+        display: flex;
+        align-items: center;
+        font-size: 14px;
+        height: 40px;
+        border-radius: 4px;
+        margin: 6px 0;
+    }
+
+    .member-name,
+    .member-jurisdiction {
+        width: 200px;
+        display: flex;
+
+        .member-jurisdiction-container {
+            display: flex;
+            justify-content: center;
+
+            .filterlist {
+                position: absolute;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                list-style-type: none;
+                font-size: 14px;
+                font-weight: 500;
+                margin: 0;
+                padding: 0 8px;
+                margin-top: 24px;
+                border-radius: 4px;
+                background-color: white;
+                box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+                z-index: 2;
+
+                .item {
+                    cursor: pointer;
+                    line-height: 32px;
+
+                    .choose {
+                        box-sizing: border-box;
+                        width: 10px;
+                        height: 6px;
+                        margin-right: 4px;
+                        margin-left: 2px;
+                        border-width: 0 0 1px 1px;
+                        border-style: solid;
+                        border-color: rgb(0, 0, 0, .75);
+                        transform: rotate(-45deg) translateY(-30%);
+                    }
+                }
+            }
+
             .shrink {
                 width: 16px;
                 height: 16px;
@@ -155,53 +330,6 @@ onUnmounted(() => {
                 }
             }
 
-            .filterlist {
-                position: fixed;
-                list-style-type: none;
-                font-size: 14px;
-                font-weight: 500;
-                min-width: 88px;
-                margin: 0;
-                padding: 0 8px;
-                margin-top: 8px;
-                border-radius: 4px;
-                background-color: white;
-                box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-
-                .choose {
-                    box-sizing: border-box;
-                    width: 10px;
-                    height: 6px;
-                    margin-right: 4px;
-                    margin-left: 2px;
-                    border-width: 0 0 1px 1px;
-                    border-style: solid;
-                    border-color: rgb(0, 0, 0, .75);
-                    transform: rotate(-45deg) translateY(-30%);
-                }
-
-                .item {
-                    cursor: pointer;
-                    display: flex;
-                    align-items: center;
-                    line-height: 32px;
-                }
-            }
-
-        }
-    }
-
-    .member-item {
-        display: flex;
-        align-items: center;
-        font-size: 14px;
-        height: 40px;
-        border-radius: 4px;
-        margin: 6px 0;
-
-        .member-name,
-        .member-jurisdiction {
-            width: 200px;
         }
     }
 }
