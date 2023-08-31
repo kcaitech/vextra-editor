097410348357023aaba6d96833d24e60370e755b
ice <icezobo12@gmail.com>
2023-08-31 16:06:51 +0800

    提交代码

--- src/components/AppHome/Aside.vue
+++ src/components/AppHome/Aside.vue
@@ -35,7 +35,7 @@ const showoverlay = ref(false);
 const teamcard = ref(false);
 const projectcard = ref(false);
 const teamid = ref('');
-const activeNames = ref([0]);
+const activeNames = ref([-1]);
 const teamList = ref<any>([]);
 
 const { teamData, updatestate, updateShareData, upDateTeamData, state, saveProjectData } = inject('shareData') as {
@@ -240,45 +240,42 @@ onUnmounted(() => {
             <div class="teamlists">
                 <div class="demo-collapse">
                     <el-collapse v-model="activeNames">
-                        <div v-for="({ team: { name, id, avatar, description }, self_perm_type, children }, index) in teamList"
-                            :key="id" @click.stop="torouter(id)">
-                            <el-collapse-item :name="index">
-                                <template #title>
-                                    <div class="team-title"
-                                        :class="{ 'is_active': isActive(id, name, avatar, description, self_perm_type) }">
-                                        <div class="left">
-                                            <div class="down"
-                                                :style="{ transform: activeNames.includes(index) ? 'rotate(0deg)' : 'rotate(-90deg)' }">
-                                                <svg-icon icon-class="down" />
-                                            </div>
-                                            <div class="team-avatar">
-                                                <div v-if="avatar.includes('http')" class="img">
-                                                    <img :src="avatar" alt="team avatar">
-                                                </div>
-                                                <div v-else class="text">
-                                                    <span>{{ name.slice(0, 1) }}</span>
-                                                </div>
-                                            </div>
-                                            <span class="name">{{ name }}</span>
+                        <el-collapse-item v-for="(data, index) in teamList" :key="data.team.id" :name="index"
+                            @click.stop="torouter(data.team.id)">
+                            <template #title>
+                                <div class="team-title"
+                                    :class="{ 'is_active': isActive(data.team.id, data.team.name, data.team.avatar, data.team.description, data.self_perm_type) }">
+                                    <div class="left">
+                                        <div class="down"
+                                            :style="{ transform: activeNames.includes(index) ? 'rotate(0deg)' : 'rotate(-90deg)' }">
+                                            <svg-icon icon-class="down" />
                                         </div>
-                                        <div class="right" @click.stop="showprojectcard(id)">
-                                            <svg-icon icon-class="close" />
+                                        <div class="team-avatar">
+                                            <div v-if="data.team.avatar.includes('http')" class="img">
+                                                <img :src="data.team.avatar" alt="team avatar">
+                                            </div>
+                                            <div v-else class="text">
+                                                <span>{{ data.team.name.slice(0, 1) }}</span>
+                                            </div>
                                         </div>
+                                        <div class="name">{{ data.team.name }}</div>
                                     </div>
-                                </template>
-                                <div class="project" v-for="(item, i) in children" :key="i"
-                                    @click.stop="skipProject(item.project.id)"
-                                    :class="{ 'is_active': isProjectActive(item.project.id) }">
-                                    <div>
-                                        <div>{{ item.project.name }}</div>
-                                        <div class="right" @click.stop="newProjectFile(item.project.id)">
-                                            <svg-icon icon-class="close" />
-                                        </div>
+                                    <div class="right" @click.stop="showprojectcard(data.team.id)">
+                                        <svg-icon icon-class="close" />
                                     </div>
                                 </div>
-
-                            </el-collapse-item>
-                        </div>
+                            </template>
+                            <div class="project" v-for="(item, i) in data.children" :key="i"
+                                @click.stop="skipProject(item.project.id)"
+                                :class="{ 'is_active': isProjectActive(item.project.id) }">
+                                <div>
+                                    <div>{{ item.project.name }}</div>
+                                    <div class="right" @click.stop="newProjectFile(item.project.id)">
+                                        <svg-icon icon-class="close" />
+                                    </div>
+                                </div>
+                            </div>
+                        </el-collapse-item>
                     </el-collapse>
                 </div>
             </div>
@@ -528,6 +525,146 @@ a {
                 width: 0;
             }
 
+            .demo-collapse {
+                .team-title {
+                    width: 100%;
+                    height: 40px;
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                    border-radius: 4px;
+                    margin-bottom: 5px;
+
+                    &:hover {
+                        background-color: #f3f0ff;
+                        .right{
+                            visibility:visible;
+                        }
+                    }
+
+                    .left {
+                        display: flex;
+                        align-items: center;
+                        width: 200px;
+                        margin-left: 8px;
+
+                        .down {
+                            display: flex;
+                            align-items: center;
+                            justify-content: center;
+                            height: 100%;
+                            margin-right:4px;
+                            margin-left: 2px;
+                            transition: .3s;
+
+                            svg {
+                                width: 10px;
+                                height: 10px;
+                            }
+                        }
+
+                        .team-avatar {
+                            width: 24px;
+                            height: 24px;
+                            min-width: 24px;
+                            background-color: #9775fa;
+                            text-align: center;
+                            border-radius: 50%;
+                            overflow: hidden;
+                            display: flex;
+                            align-items: center;
+                            justify-content: center;
+                            margin-right: 6px;
+
+                            .img {
+                                width: 100%;
+                                height: 100%;
+                                line-height: 0;
+
+                                img {
+                                    width: 100%;
+                                    height: 100%;
+                                    object-fit: cover;
+                                }
+                            }
+
+                            .text {
+                                display: flex;
+
+                                span {
+                                    font-size: 12px;
+                                    font-weight: 600;
+                                    color: white;
+                                }
+                            }
+                        }
+
+                        .name {
+                            overflow: hidden;
+                            text-overflow: ellipsis;
+                            white-space: nowrap;
+                        }
+                    }
+
+                    .right {
+                        visibility: hidden;
+                        margin-right: 8px;
+                        height: 100%;
+
+                        svg {
+                            width: 16px;
+                            min-width: 16px;
+                            height: 16px;
+                            fill: #9775fa;
+                            transform: rotate(45deg);
+                        }
+                    }
+                }
+
+                .project {
+                    width: 100%;
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+                    border-radius: 4px;
+                    cursor: pointer;
+
+                    &:hover {
+                        background-color: #f3f0ff;
+
+                        .right {
+                            visibility: visible;
+                        }
+                    }
+
+                    >div {
+                        display: flex;
+                        align-items: center;
+                        justify-content: space-between;
+                        width: 100%;
+                        height: 35px;
+                        border-radius: 4px;
+                        padding-left: 50px;
+
+
+                        .right {
+                            display: flex;
+                            align-items: center;
+                            visibility: hidden;
+                            height: 100%;
+                            padding-right: 10px;
+
+                            svg {
+                                width: 16px;
+                                min-width: 16px;
+                                height: 16px;
+                                fill: #9775fa;
+                                transform: rotate(45deg);
+                            }
+                        }
+                    }
+                }
+            }
 
             .teamitem {
                 border-radius: 4px;
@@ -549,148 +686,7 @@ a {
     }
 }
 
-.team-title {
-    width: 100%;
-    height: 40px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border-radius: 4px;
-    margin-bottom: 5px;
 
-    &:hover {
-        background-color: #f3f0ff;
-    }
-
-    .left {
-        flex: 1;
-        display: flex;
-        align-items: center;
-        height: 40px;
-
-        .down {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            width: 20px;
-            height: 100%;
-            margin-right: 3px;
-            margin-left: 2px;
-            transition: .3s;
-
-            svg {
-                width: 8px;
-                height: 8px;
-            }
-        }
-
-        .team-avatar {
-            width: 24px;
-            height: 24px;
-            min-width: 24px;
-            background-color: #9775fa;
-            text-align: center;
-            border-radius: 50%;
-            overflow: hidden;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            margin-right: 6px;
-
-            .img {
-                width: 100%;
-                height: 100%;
-                line-height: 0;
-
-                img {
-                    width: 100%;
-                    height: 100%;
-                    object-fit: cover;
-
-                }
-            }
-
-            .text {
-                height: 100%;
-
-                span {
-                    width: 100%;
-                    height: 100%;
-                    font-size: 12px;
-                    font-weight: 600;
-                    color: white;
-                }
-            }
-        }
-
-        .name {
-            display: flex;
-            align-items: center;
-            height: 100%;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            white-space: nowrap;
-        }
-    }
-
-    .right {
-        visibility: hidden;
-        height: 40px;
-        margin-right: 10px;
-
-        svg {
-            width: 16px;
-            min-width: 16px;
-            height: 16px;
-            fill: #9775fa;
-            transform: rotate(45deg);
-        }
-    }
-}
-
-.project {
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    border-radius: 4px;
-    cursor: pointer;
-
-    &:hover {
-        background-color: #f3f0ff;
-
-        .right {
-            visibility: visible;
-        }
-    }
-
-    >div {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        width: 100%;
-        height: 35px;
-        border-radius: 4px;
-        padding-left: 50px;
-
-
-        .right {
-            display: flex;
-            align-items: center;
-            visibility: hidden;
-            height: 100%;
-            padding-right: 10px;
-
-            svg {
-                width: 16px;
-                min-width: 16px;
-                height: 16px;
-                fill: #9775fa;
-                transform: rotate(45deg);
-            }
-        }
-    }
-}
 
 :deep(.el-collapse-item__content) {
     padding: 0;
