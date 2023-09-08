7b689b11f0da89d2da19b50d3cb284cbdeee8c0a
Zrx <georgezrx@163.com>
2023-09-08 09:50:08 +0800

    fix

--- src/components/Document/Selection/Controller/CtrlContact.vue
+++ src/components/Document/Selection/Controller/CtrlContact.vue
@@ -22,6 +22,20 @@ const editing = ref<boolean>(false);
 const matrix = new Matrix();
 const submatrix = reactive(new Matrix());
 // #region 绘制控件
+const bounds = reactive({ left: 0, top: 0, right: 0, bottom: 0 });
+
+let viewBox = '';
+const width = computed(() => {
+    const w = bounds.right - bounds.left;
+    return w < 10 ? 10 : w;
+})
+const height = computed(() => {
+    const h = bounds.bottom - bounds.top;
+    return h < 10 ? 10 : h;
+})
+function genViewBox(bounds: { left: number, top: number, right: number, bottom: number }) {
+    return "" + bounds.left + " " + bounds.top + " " + width.value + " " + height.value;
+}
 function updateControllerView() {
     const m2p = props.shape.matrix2Root();
     matrix.reset(m2p);
@@ -30,13 +44,27 @@ function updateControllerView() {
     const path = props.shape.getPath();
     path.transform(matrix);
     props.context.workspace.setCtrlPath(path.toString());
+    const framePoint = props.controllerFrame;
+    const p0 = framePoint[0];
+    bounds.left = p0.x;
+    bounds.top = p0.y;
+    bounds.right = p0.x;
+    bounds.bottom = p0.y;
+    framePoint.reduce((bounds, point) => {
+        if (point.x < bounds.left) bounds.left = point.x;
+        else if (point.x > bounds.right) bounds.right = point.x;
+        if (point.y < bounds.top) bounds.top = point.y;
+        else if (point.y > bounds.bottom) bounds.bottom = point.y;
+        return bounds;
+    }, bounds);
+    viewBox = genViewBox(bounds);
 }
 // #endregion
 function selection_watcher(t: number) {
     if (t == Selection.CHANGE_SHAPE) editing.value = false;
 }
 function workspace_watcher(t: number) {
-    if (t === WorkSpace.TRANSLATING) visible.value = ! props.context.workspace.isTranslating;
+    if (t === WorkSpace.TRANSLATING) visible.value = !props.context.workspace.isTranslating;
     else if (t === WorkSpace.PRE_EDIT) editing.value = props.context.workspace.isEditing;
 }
 onMounted(() => {
@@ -52,8 +80,9 @@ watchEffect(updateControllerView);
 </script>
 <template>
     <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" data-area="controller"
-        xmlns:xhtml="http://www.w3.org/1999/xhtml" preserveAspectRatio="xMinYMin meet" viewBox="0 0 10 10" width="10"
-        height="10" overflow="visible" style=" position: absolute">
+        xmlns:xhtml="http://www.w3.org/1999/xhtml" preserveAspectRatio="xMinYMin meet" overflow="visible"
+        style=" position: absolute" :viewBox="viewBox" :width="width" :height="height"
+        :style="{ transform: `translate(${bounds.left}px,${bounds.top}px)`, left: 0, top: 0, position: 'absolute' }">
         <ContactApex :context="props.context" :matrix="submatrix.toArray()" :shape="props.shape"
             :c-frame="props.controllerFrame"></ContactApex>
     </svg>
