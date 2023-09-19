f45ed61bcf9710345a2f5025fce87414ffc0c5e3
Kc_tjs <3288771283@qq.com>
2023-09-19 09:08:32 +0800

    v

--- src/components/Document/Attribute/Module/ComponentAttr.vue
+++ src/components/Document/Attribute/Module/ComponentAttr.vue
@@ -12,11 +12,13 @@ const compsType = ref(false)
 const selectComps = ref<HTMLDivElement>()
 const attrInput = ref('属性1')
 const showRename = ref(false)
-const renameInput = ref<HTMLInputElement>()
 const isLayerShow = ref(false)
+const moduleStates = ref<any[]>([])
+const showStates = ref<any[]>([])
+const stateIndex = ref(-1)
 
 const selectCompsType = () => {
-    if(compsType.value) {
+    if (compsType.value) {
         return compsType.value = false
     }
     compsType.value = true
@@ -30,24 +32,27 @@ const closeCompsType = (e: Event) => {
     document.removeEventListener('mousedown', closeCompsType)
 }
 
-const onRename = () => {
+const onRename = (index: number) => {
+    stateIndex.value = index;
+    attrInput.value = moduleStates.value[index].attrName;
     showRename.value = true
     nextTick(() => {
-        if(renameInput.value) {
-            renameInput.value?.focus()
-            renameInput.value?.select()
+        const el = document.querySelector(`[data-area="state-${index}"]`);
+        if (el) {
+            (el as HTMLInputElement).focus();
         }
     })
+
 }
 
 const closeInput = () => {
-    if(attrInput.value.trim().length === 0) return showRename.value = false
+    if (attrInput.value.trim().length === 0) return showRename.value = false
     showRename.value = false
 }
 
 const editName = (e: KeyboardEvent) => {
-    if(attrInput.value.trim().length === 0) return showRename.value = false
-    if(e.code === 'Enter' || e.code === 'NumpadEnter') {
+    if (attrInput.value.trim().length === 0) return showRename.value = false
+    if (e.code === 'Enter' || e.code === 'NumpadEnter') {
         showRename.value = false
     }
 }
@@ -60,52 +65,93 @@ const closeLayerShowPopup = () => {
     isLayerShow.value = false
 }
 
+const addModuleState = () => {
+    const len = moduleStates.value.length + 1;
+    const state = {
+        attrName: '属性' + len,
+        attrValue: '默认'
+    }
+    moduleStates.value.push(state);
+}
+
+const selectAllText = (event: FocusEvent) => {
+    (event.target as HTMLInputElement).select(); // 选择输入框内的文本
+};
+
 </script>
 
 <template>
-    <TypeHeader :title="'组件属性'" class="mt-24">
-        <template #tool>
-            <div class="add-comps" @click="selectCompsType">
-                <svg-icon icon-class="add"></svg-icon>
-                <div class="selectType" v-if="compsType" ref="selectComps">
-                    <div class="type-title">请选择属性类型:</div>
-                    <div class="status">
-                        <svg-icon icon-class="pattern-arrow"></svg-icon>
-                        <span>组件状态</span>
-                    </div>
-                    <div class="status" @click="layerIsShow">
-                        <svg-icon icon-class="eye-open"></svg-icon>
-                        <span>显示状态</span>
-                    </div>
-                    <div class="status">
-                        <svg-icon icon-class="pattern-rectangle"></svg-icon>
-                        <span>实例切换</span>
-                    </div>
-                    <div class="status">
-                        <svg-icon icon-class="text"></svg-icon>
-                        <span>文本内容</span>
+    <div style="position: relative;">
+        <TypeHeader :title="'组件属性'" class="mt-24">
+            <template #tool>
+                <div class="add-comps" @click="selectCompsType">
+                    <svg-icon icon-class="add"></svg-icon>
+                    <div class="selectType" v-if="compsType" ref="selectComps">
+                        <div class="type-title">请选择属性类型:</div>
+                        <div class="status" @click="addModuleState">
+                            <svg-icon icon-class="pattern-arrow"></svg-icon>
+                            <span>组件状态</span>
+                        </div>
+                        <div class="status" @click="layerIsShow">
+                            <svg-icon icon-class="eye-open"></svg-icon>
+                            <span>显示状态</span>
+                        </div>
+                        <div class="status">
+                            <svg-icon icon-class="pattern-rectangle"></svg-icon>
+                            <span>实例切换</span>
+                        </div>
+                        <div class="status">
+                            <svg-icon icon-class="text"></svg-icon>
+                            <span>文本内容</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </template>
-    </TypeHeader>
-    <CompLayerShow :context="context" v-if="isLayerShow" @close="closeLayerShowPopup"></CompLayerShow>
-    <div class="module_container">
-        <div class="module_attr_item">
-            <div class="attr_con">
-                <div class="module_item_left" @dblclick="onRename" v-if="!showRename">
-                    <div class="module_name">
-                        <svg-icon icon-class="pattern-arrow"></svg-icon>
-                        <span>属性1</span>
+            </template>
+        </TypeHeader>
+        <CompLayerShow :context="context" v-if="isLayerShow" @close-dialog="closeLayerShowPopup" right="250px" :width="260" :title="`图层是否显示`"></CompLayerShow>
+        <div class="module_container">
+            <template v-for="(item, index) in moduleStates" :key="index">
+                <div class="module_attr_item">
+                    <div class="attr_con">
+                        <div class="module_input" v-if="showRename && stateIndex === index"><el-input v-model="attrInput"
+                                @focus="selectAllText" class="input" :data-area="'state-' + index" @blur="closeInput"
+                                @keydown="editName" />
+                        </div>
+                        <div class="module_item_left" @dblclick="onRename(index)" v-else>
+                            <div class="module_name">
+                                <svg-icon icon-class="pattern-arrow"></svg-icon>
+                                <span>{{ item.attrName }}</span>
+                            </div>
+                            <div><span>{{ item.attrValue }}</span></div>
+                        </div>
+                        <div class="delete">
+                            <svg-icon icon-class="delete"></svg-icon>
+                        </div>
                     </div>
-                    <div><span>默认</span></div>
+                    <div class="warn" v-if="false">名称重复，请重新输入</div>
                 </div>
-                <div class="module_input" v-if="showRename"><el-input v-model="attrInput" ref="renameInput" @blur="closeInput" @keydown="editName"/></div>
-                <div class="delete">
-                    <svg-icon icon-class="delete"></svg-icon>
+            </template>
+            <template v-for="(item, index) in showStates" :key="index">
+                <div class="module_attr_item">
+                    <div class="attr_con">
+                        <div class="module_input" v-if="showRename && stateIndex === index"><el-input v-model="attrInput"
+                                @focus="selectAllText" class="input" :data-area="'show-' + index" @blur="closeInput"
+                                @keydown="editName" />
+                        </div>
+                        <div class="module_item_left" @dblclick="onRename(index)" v-else>
+                            <div class="module_name">
+                                <svg-icon icon-class="eye-open"></svg-icon>
+                                <span>{{ item.attrName }}</span>
+                            </div>
+                            <div><span>{{ item.attrValue }}</span></div>
+                        </div>
+                        <div class="delete">
+                            <svg-icon icon-class="delete"></svg-icon>
+                        </div>
+                    </div>
+                    <div class="warn" v-if="false">名称重复，请重新输入</div>
                 </div>
-            </div>
-            <div class="warn" v-if="showRename">名称重复，请重新输入</div>
+            </template>
         </div>
     </div>
 </template>
@@ -123,6 +169,7 @@ const closeLayerShowPopup = () => {
         width: 50%;
         height: 50%;
     }
+
     .selectType {
         position: absolute;
         top: 25px;
@@ -134,22 +181,26 @@ const closeLayerShowPopup = () => {
         padding: 5px 0;
         box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.2);
         z-index: 100;
+
         .type-title {
             display: flex;
             align-items: center;
             height: 30px;
             padding: 0 10px;
         }
+
         .status {
             display: flex;
             align-items: center;
             height: 25px;
             padding: 2px 10px;
+
             >svg {
                 width: 14px;
                 height: 14px;
                 margin-right: 10px;
             }
+
             &:hover {
                 background-color: var(--active-color);
                 color: #fff;
@@ -157,46 +208,57 @@ const closeLayerShowPopup = () => {
         }
     }
 }
+
 .module_container {
     font-size: var(--font-default-fontsize);
     margin-bottom: 10px;
+
     .module_attr_item {
         display: flex;
         flex-direction: column;
+        margin-bottom: 5px;
+
         .attr_con {
             display: flex;
             align-items: center;
             justify-content: space-between;
         }
+
         .module_item_left {
             display: flex;
             align-items: center;
             background-color: #ccc;
             width: 100%;
             height: 30px;
+
             .module_name {
                 width: 45%;
-                >svg{
+
+                >svg {
                     width: 10px;
                     height: 10px;
                     margin: 0 10px;
                 }
             }
         }
+
         .module_input {
             display: flex;
             align-items: center;
             width: 100%;
             height: 30px;
+
             .el-input {
                 font-size: 10px;
-                height: 25px;
+                height: 30px;
             }
         }
+
         .warn {
-            margin-left: 10px;
             color: red;
+            transform: scale(.9);
         }
+
         .delete {
             flex: 0 0 22px;
             display: flex;
@@ -214,4 +276,11 @@ const closeLayerShowPopup = () => {
         }
     }
 }
-</style>
+
+:deep(.el-input__inner) {
+    --el-input-inner-height: 100%;
+}
+
+:deep(.el-input__wrapper.is-focus) {
+    box-shadow: 0 0 0 1px var(--active-color) inset;
+}</style>
--- src/components/Document/Attribute/PopoverMenu/CompLayerShow.vue
+++ src/components/Document/Attribute/PopoverMenu/CompLayerShow.vue
@@ -1,85 +1,142 @@
-<script lang="ts" setup>
-import { useI18n } from 'vue-i18n';
+<script setup lang="ts">
+import { ref, nextTick, onMounted, onUnmounted } from 'vue';
 import { Context } from '@/context';
-import TypeHeader from '../TypeHeader.vue';
-import { ref, nextTick } from 'vue'
-const { t } = useI18n();
 const props = defineProps<{
-    context: Context
-}>()
+    title?: string,
+    top?: string,
+    right?: string,
+    width?: number,
+    height?: string | number,
+    context: Context;
+}>();
 const emit = defineEmits<{
-    (e: 'close'): void
+    (e:'closeDialog'): void;
 }>()
 
-const popoverClose = () => {
-    emit('close')
+function popoverClose() {
+    emit('closeDialog');
 }
+function esc(e: KeyboardEvent) {
+    if (e.code === 'Escape') popoverClose();
+    else e.stopPropagation();
+}
+
+onMounted(() => {
+    document.addEventListener('keyup', esc);
+})
+onUnmounted(() => {
+    document.removeEventListener('keyup', esc);
+})
 </script>
 
 <template>
-    <div class="compLayer-container" tabindex="-1">
-        <div class="popover" ref="popover">
-            <div class="header">
-                <span class="title">图层是否显示</span>
-                <div @click="popoverClose" class="close">
-                    <svg-icon icon-class="close"></svg-icon>
-                </div>
+    <div class="dialog_box" :style="{
+        width: `${props.width ? props.width : 360}px`,
+        right: props.right,
+        top: props.top
+    }">
+        <div class="header">
+            <span class="title">{{ props.title }}</span>
+            <div @click="popoverClose" class="close">
+                <svg-icon icon-class="close"></svg-icon>
             </div>
-            <div class="body"></div>
         </div>
-  </div>
+        <div class="body">
+            <div>
+                <span></span>
+                <div></div>
+            </div>
+            <div>
+                <span></span>
+                <div></div>
+            </div>
+            <div>
+                <span></span>
+                <div></div>
+            </div>
+        </div>
+        <div class="footer">
+            <el-button style="background-color: #9775fa;">确认</el-button>
+        </div>
+    </div>
+    <div class="overlay" @click.stop @mousedown.stop @wheel.stop></div>
 </template>
 
-<style lang="scss" scoped>
-.compLayer-container {
-    position: relative;
+<style scoped lang="scss">
+.dialog_box {
+    position: absolute;
     outline: none;
-    z-index: 99;
-    .popover {
-        position: absolute;
-        width: 360px;
-        height: 300px;
-        outline: none;
-        box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.2);
-        background-color: #ffffff;
-        z-index: 1;
-        border-radius: 4px;
-        .header {
-            width: 100%;
-            height: 32px;
-            border-bottom: 1px solid var(--grey-light);
+    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.2);
+    background-color: #ffffff;
+    border-radius: 4px;
+    z-index: 10004;
+
+    .header {
+        width: 100%;
+        height: 32px;
+        border-bottom: 1px solid var(--grey-light);
+        display: flex;
+        font-size: var(--font-default-fontsize);
+        padding: 0 var(--default-padding);
+        box-sizing: border-box;
+        align-items: center;
+
+        .title {
+            line-height: 32px;
+            font-weight: var(--font-default-bold);
+        }
+
+        .close {
+            width: 24px;
+            height: 24px;
+            position: absolute;
+            right: var(--default-padding);
             display: flex;
-            font-size: var(--font-default-fontsize);
-            padding: 0 var(--default-padding);
-            box-sizing: border-box;
             align-items: center;
+            justify-content: center;
 
-            >.title {
-                line-height: 32px;
-                font-weight: var(--font-default-bold);
-            }
-
-            >.close {
-                width: 24px;
-                height: 24px;
-                position: absolute;
-                right: var(--default-padding);
-                display: flex;
-                align-items: center;
-                justify-content: center;
-
-                >svg {
+            >svg {
                 width: 65%;
                 height: 65%;
-                }
             }
         }
-        .body {
-            width: 100%;
-            height: calc(100% - 32px);
-            font-size: 10px;
-            position: relative;
-        }
     }
+    .body {
+      width: 100%;
+      height: calc(100% - 32px);
+      font-size: 10px;
+      >div {
+        height: 30px;
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      }
+    }
+    .footer {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin: 10px 0;
+    }
+}
+:deep(.el-button:focus, .el-button:hover) {
+    background-color: #9775fa;
+    border-color: #9775fa;
+    color: #fff;
+    outline: none;
+}
+:deep(.el-button) {
+    color: #fff;
+}
+.overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 10003;
+    background-color: transparent;
 }
 </style>
