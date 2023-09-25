a2b5ba4d612206762574d6751590014659925d99
zrx <georgezrx@163.com>
2023-09-25 23:09:03 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,46 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import { debounce } from 'lodash';
+import { onMounted } from 'vue';
 
 const _ResizeObserver = window.ResizeObserver;
-window.ResizeObserver = class ResizeObserver extends _ResizeObserver{
-  constructor(callback:any) {
+window.ResizeObserver = class ResizeObserver extends _ResizeObserver {
+  constructor(callback: any) {
     callback = debounce(callback, 16);
     super(callback);
   }
 }
+onMounted(() => {
+  document.title = 'ProtoDesign';
+})
 </script>
 
 <template>
-    <RouterView></RouterView>
+  <RouterView></RouterView>
 </template>
 
 <style lang="scss">
-html,body {
-    margin: 0;
-    padding: 0;
-    >body {
-        font-family: var(--font-family);
-        height: 100vh;
-        user-select: none; //禁止复制内容
-        position: relative;
+html,
+body {
+  margin: 0;
+  padding: 0;
 
-        &::-webkit-scrollbar {
-            height: 0;
-            width: 0;
-        }
+  >body {
+    font-family: var(--font-family);
+    height: 100vh;
+    user-select: none; //禁止复制内容
+    position: relative;
 
-        >#app {
-           position: absolute;
-           width: 100%;
-           height: 100%;
-        }
+    &::-webkit-scrollbar {
+      height: 0;
+      width: 0;
     }
+
+    >#app {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+    }
+  }
 }
 </style>
