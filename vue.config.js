733e3eab295726626236749270eb685dc20de24b
Kc_tjs <3288771283@qq.com>
2023-10-11 16:56:07 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -124,9 +124,9 @@ var configureWebpack = (config) => {
 
 var exports = defineConfig({
     transpileDependencies: true,
-    publicPath: '/tjs',
+    // publicPath: '/tjs',
     // publicPath: '/zbb',
-    // publicPath: './',
+    publicPath: './',
     configureWebpack,
 
     pluginOptions: {
