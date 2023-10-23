6a74ad6d782d85cf3f5420a93aa1dc1f64126f39
Zrx <georgezrx@163.com>
2023-10-23 16:11:19 +0800

    fix

--- src/components/Document/ContentStatic/static_coms_map.ts
+++ src/components/Document/ContentStatic/static_coms_map.ts
@@ -3,6 +3,7 @@ import RectangleStatic from "./RectangleStatic.vue";
 import ShapeGroupStatic from "./ShapeGroupStatic.vue";
 
 export const static_coms_map = new Map<ShapeType, any>([
-    [ShapeType.Rectangle, RectangleStatic],
-    [ShapeType.Group, ShapeGroupStatic]
+    [ShapeType.Rectangle, RectangleStatic], // 矩形
+    [ShapeType.Oval, RectangleStatic], // 圆形
+    [ShapeType.Group, ShapeGroupStatic] // 编组
 ])
