ecf42ce983c3c13b063974468e66d624465229c5
Zrx <georgezrx@163.com>
2023-10-23 17:13:07 +0800

    fix

---
+++ src/components/Document/ContentStatic/TextViewStatic.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {TextShape, RenderTransform, SymbolRefShape, SymbolShape, Variable} from '@kcdesign/data';
+import { h } from 'vue';
+import { renderTextStatic as r } from "@kcdesign/data"
+interface Props {
+    data: TextShape
+    transx?: RenderTransform
+    varsContainer?: (SymbolRefShape | SymbolShape)[]
+}
+const props = defineProps<Props>();
+function render() {
+    const consumedVars: { slot: string, vars: Variable[] }[] = [];
+    return r(h, props.data, props.transx, props.varsContainer, consumedVars);
+}
+</script>
+
+<template>
+    <render />
+</template>
--- src/components/Document/ContentStatic/static_coms_map.ts
+++ src/components/Document/ContentStatic/static_coms_map.ts
@@ -1,9 +1,14 @@
 import {ShapeType} from "@kcdesign/data";
 import RectangleStatic from "./RectangleStatic.vue";
 import ShapeGroupStatic from "./ShapeGroupStatic.vue";
-
+import TextViewStatic from "./TextViewStatic.vue";
+import ImageViewStatic from "./ImageViewStatic.vue";
+import ArtboardViewStatic from "./ArtboardViewStatic.vue";
 export const static_coms_map = new Map<ShapeType, any>([
     [ShapeType.Rectangle, RectangleStatic], // 矩形
     [ShapeType.Oval, RectangleStatic], // 圆形
-    [ShapeType.Group, ShapeGroupStatic] // 编组
+    [ShapeType.Group, ShapeGroupStatic], // 编组
+    [ShapeType.Text, TextViewStatic], // 文字
+    [ShapeType.Image, ImageViewStatic], // 图片
+    [ShapeType.Artboard, ArtboardViewStatic] // 容器
 ])
