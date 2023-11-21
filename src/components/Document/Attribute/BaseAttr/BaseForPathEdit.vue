2301af467c2c9b1d1c7ccabff1c033afb967bbf8
Zrx <georgezrx@163.com>
2023-11-21 16:56:01 +0800

    路径编辑输入框送值

--- src/components/Document/Attribute/BaseAttr/BaseForPathEdit.vue
+++ src/components/Document/Attribute/BaseAttr/BaseForPathEdit.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import IconText from "@/components/common/IconText.vue";
-import {ref, reactive} from "vue";
+import {ref, reactive, onMounted, onUnmounted} from "vue";
 import {Context} from "@/context";
 import {useI18n} from 'vue-i18n';
 import SvgIcon from "@/components/common/SvgIcon.vue";
 import Tooltip from "@/components/common/Tooltip.vue";
+import {Path} from "@/context/path";
+import {get_action_for_key_change, get_value_from_point, get_value_from_points} from "@/utils/pathedit";
+import {PathShape} from "@kcdesign/data";
+import {Selection} from "@/context/selection";
 
 interface Props {
     context: Context
@@ -18,16 +22,28 @@ interface ModelState {
 }
 
 const props = defineProps<Props>();
-const x = ref<number | string>(0);
-const y = ref<number | string>(0);
+const x = ref<number | string>('');
+const y = ref<number | string>('');
 const r = ref<number | string>(0);
-const model_state: ModelState = reactive({x: true, y: true, r: false, tool: true});
+const model_state: ModelState = reactive({x: true, y: true, r: true, tool: true});
 const t = useI18n().t;
+let path_shape: PathShape | undefined = undefined;
 
-function onChangeX() {
+function execute_change_xy(key: 'x' | 'y', val: any) {
+    val = Number(val);
+    if (!path_shape || isNaN(val)) return;
+    const editor = props.context.editor4Shape(path_shape);
+    const actions = get_action_for_key_change(props.context, val, key);
+    if (!actions) return;
+    editor.modifyPointsXY(actions)
 }
 
-function onChangeY() {
+function onChangeX(_val: any) {
+    execute_change_xy('x', _val);
+}
+
+function onChangeY(_val: any) {
+    execute_change_xy('y', _val);
 }
 
 function onChangeR() {
@@ -36,6 +52,90 @@ function onChangeR() {
 function exit() {
     props.context.workspace.setPathEditMode(false);
 }
+
+function calc() {
+    const selected_points = props.context.path.selectedPoints;
+    const l = selected_points.length;
+    if (l) {
+        if (l === 1) {
+            const state = get_value_from_point(props.context, selected_points[0]);
+            if (!state) return;
+            x.value = state.x;
+            y.value = state.y;
+            r.value = state.r;
+        } else {
+            const state = get_value_from_points(props.context, selected_points);
+            if (!state) return;
+            x.value = state.x === 'mix' ? t('attr.more_value') : state.x;
+            y.value = state.y === 'mix' ? t('attr.more_value') : state.y;
+            r.value = state.r;
+        }
+    } else {
+        x.value = '';
+        y.value = '';
+        r.value = 0;
+    }
+}
+
+function modify_model_state() {
+    const selected_points = props.context.path.selectedPoints;
+    const l = selected_points.length;
+    if (l) {
+        model_state.x = false;
+        model_state.y = false;
+        model_state.r = false;
+        model_state.tool = false;
+    } else {
+        model_state.x = true;
+        model_state.y = true;
+        model_state.r = true;
+        model_state.tool = true;
+    }
+}
+
+function update() {
+    modify_model_state();
+    calc();
+}
+
+function path_watcher(t: number) {
+    if (t === Path.SELECTION_CHANGE) {
+        update();
+    }
+}
+
+function init_path_shape() {
+    path_shape = props.context.selection.pathshape;
+    if (path_shape) {
+        path_shape.watch(calc);
+    }
+}
+
+function selection_watcher(t: number) {
+    if (t === Selection.CHANGE_SHAPE) {
+        const t = props.context.selection.pathshape;
+        if (path_shape) {
+            path_shape.unwatch(calc);
+        }
+        if (t) {
+            path_shape = t;
+            path_shape.watch(calc);
+        }
+    }
+}
+
+onMounted(() => {
+    props.context.path.watch(path_watcher);
+    props.context.selection.watch(selection_watcher);
+    init_path_shape();
+})
+onUnmounted(() => {
+    props.context.path.unwatch(path_watcher);
+    props.context.selection.unwatch(selection_watcher);
+    if (path_shape) {
+        path_shape.unwatch(calc);
+    }
+})
 </script>
 <template>
     <div class="table">
@@ -52,9 +152,9 @@ function exit() {
                       @onchange="onChangeR" :disabled="model_state.r" :context="context"/>
         </div>
         <div class="tr">
-            <div class="tool">
+            <div :class="{tool: true, tool_disabled: model_state.tool}">
                 <Tooltip :content="t('attr.right_angle')">
-                    <div class="item active">
+                    <div class="item">
                         <svg-icon icon-class="unknown"></svg-icon>
                     </div>
                 </Tooltip>
@@ -132,26 +232,30 @@ function exit() {
             align-items: center;
             box-sizing: border-box;
             position: relative;
-
+            transition: 0.3s;
 
             .item {
                 height: calc(100% - 4px);
                 margin: 2px;
                 border-radius: var(--default-radius);
-                background-color: var(--grey-dark);
                 display: flex;
                 justify-content: center;
                 align-items: center;
-                color: #ffffff;
 
                 > svg {
                     height: 60%;
                     width: 60%;
                 }
             }
+
             .active {
                 background-color: var(--active-color-beta);
             }
+        }
+
+        .tool_disabled {
+            opacity: 0.5;
+            pointer-events: none;
         }
 
         .btn {
