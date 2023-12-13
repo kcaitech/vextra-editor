219e8473d18b919af7cb3e5e2aca672642f2a794
ruiqiangzh <ruiqiangzh@gmail.com>
2023-12-13 17:38:45 +0800

    table

--- src/components/Document/Content/vdom/tablecell.ts
+++ src/components/Document/Content/vdom/tablecell.ts
@@ -1,6 +1,10 @@
 import { TableCellView } from "@kcdesign/data";
 import { DomBasic } from "./basic";
+import { DViewCtx, PropsType } from "@kcdesign/data";
+import { IMAGE_DEFAULT } from "../common";
 
 export class TableCellDom extends DomBasic(TableCellView) {
-
+    constructor(ctx: DViewCtx, props: PropsType) {
+        super(ctx, props, IMAGE_DEFAULT);
+    }
 }
