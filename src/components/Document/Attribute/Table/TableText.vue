1a4dd99c03ea5b78f64a03bccf9a271181afb82b
tyx <474506691@qq.com>
2023-12-18 15:29:17 +0800

    文本样式

--- src/components/Document/Attribute/Table/TableText.vue
+++ src/components/Document/Attribute/Table/TableText.vue
@@ -931,21 +931,23 @@ onUnmounted(() => {
     border-bottom: 1px solid #F0F0F0;
 
     .trigger {
-        width: 100%;
-        height: 100%;
+        width: 28px;
+        height: 28px;
         display: flex;
         justify-content: center;
         align-items: center;
+        margin-top: -11px;
+        margin-left: -12px;
+        border-radius: var(--default-radius);
 
         >svg {
             width: 16px;
             height: 16px;
-            transition: 0.3s;
         }
+    }
 
-        svg:hover {
-            transform: rotate(90deg);
-        }
+    .trigger:hover {
+        background-color: #F5F5F5;
     }
 
     .text-container {
