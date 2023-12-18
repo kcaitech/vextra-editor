6f81388b2736f83842d18ff593b1fbde9b0fce8c
Zrx <georgezrx@163.com>
2023-12-18 09:34:11 +0800

    二级菜单调整

--- src/components/Document/Toolbar/index.vue
+++ src/components/Document/Toolbar/index.vue
@@ -24,6 +24,6 @@ const selection = shallowRef(props.context.selection);
     display: flex;
     align-items: center;
     justify-content: space-between;
-    min-width: 1160px
+    min-width: 960px
 }
 </style>
