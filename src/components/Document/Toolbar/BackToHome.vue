26724baebef79fc3aa1e2a729376c04467a85083
ice <icezobo12@gmail.com>
2023-12-19 10:06:42 +0800

    提交代码

--- src/components/Document/Toolbar/BackToHome.vue
+++ src/components/Document/Toolbar/BackToHome.vue
@@ -161,7 +161,6 @@ onUnmounted(() => {
 })
 </script>
 <template>
-<<<<<<< HEAD
   <div class="container" @dblclick.stop>
     <div class="home" @click="home">
       <!--            <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">-->
@@ -171,29 +170,6 @@ onUnmounted(() => {
       <!--                </path>-->
       <!--            </svg>-->
       <svg-icon icon-class="home"></svg-icon>
-=======
-    <div class="container" @dblclick.stop>
-        <div class="home" @click="home">
-<!--            <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">-->
-<!--                <path-->
-<!--                    d="M946.751948 410.824772L527.008111 79.821076a28.047291 28.047291 0 0 0-34.717904 0L72.547395 410.824772c-12.15659 9.583567-14.25146 27.21692-4.654583 39.374534 9.596877 12.19857 27.271186 14.252484 39.37351 4.654582l48.518854-38.260546v513.949477c0 15.484218 12.540547 28.0391 28.038076 28.0391h651.65591c15.497528 0 28.038076-12.554882 28.038076-28.0391v-513.949477l48.51783 38.260546a27.917257 27.917257 0 0 0 17.331307 6.023518c8.296543 0 16.510151-3.655271 22.041179-10.679124 9.581519-12.15659 7.500984-29.789944-4.655606-39.37351z m-553.250292 490.379637V668.85616h232.323676v232.347225H393.501656z m413.937383 1.300334H681.899435V640.81706c0-15.484218-12.540547-28.0391-28.038075-28.039099h-288.39778c-15.497528 0-28.037052 12.554882-28.037052 28.039099v261.687683H211.85928V372.373782L509.649159 137.541587 807.439039 372.373782v530.130961z"-->
-<!--                    fill="#ffffff">-->
-<!--                </path>-->
-<!--            </svg>-->
-            <svg-icon icon-class="home"></svg-icon>
-        </div>
-        <DocumentMenu :context="props.context" @rename="rename"></DocumentMenu>
-        <div class="rename">
-            <span v-if="ele === 1" @click="rename">{{ name }}</span>
-            <input v-if="ele === 2" type="text" ref="input"/>
-            <div class="save" v-if="ele === 3">
-                <Saving></Saving>
-            </div>
-        </div>
-        <div class="model">
-            <span v-if="isLable" style="color: #fff; font-size: 12px">【开发模式】</span>
-        </div>
->>>>>>> a773a4b843c509a0d50ff457efbdce53dfe2f17b
     </div>
     <DocumentMenu :context="props.context" @rename="rename"></DocumentMenu>
     <div class="rename">
@@ -203,7 +179,9 @@ onUnmounted(() => {
         <Saving></Saving>
       </div>
     </div>
-    <span v-if="isLable" style="margin-left: 10px; color: #fff; font-size: 12px; width: 35%">【开发模式】</span>
+    <div class="model">
+      <span v-if="isLable" style="color: #fff; font-size: 12px">【开发模式】</span>
+    </div>
   </div>
   <Teleport to="body">
     <ProjectDialog :projectVisible="showbackhometips" :context="t('message.unuploaded_msg')"
@@ -214,21 +192,15 @@ onUnmounted(() => {
 
 <style scoped lang="scss">
 .container {
-<<<<<<< HEAD
+  display: flex;
   align-items: center;
-  justify-content: space-around;
-  width: 116px;
+  justify-content: space-between;
+  width: auto;
   height: 32px;
 
   .home {
     cursor: pointer;
     width: 32px;
-=======
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: auto;
->>>>>>> a773a4b843c509a0d50ff457efbdce53dfe2f17b
     height: 32px;
     display: flex;
     align-items: center;
@@ -237,42 +209,32 @@ onUnmounted(() => {
     border-radius: 4px;
 
     >svg {
-      width: 32px;
-      height: 32px;
+      width: 18px;
+      height: 18px;
       color: #FFFFFF;
     }
   }
 
-<<<<<<< HEAD
   .home:hover {
     background-color: rgba(255, 255, 255, 0.1);
   }
 
   .rename {
-    width: 224px;
+    max-width: 224px;
     height: 52px;
     flex: 1;
-    margin-left: 64px;
-    margin-top: -44px;
     box-sizing: border-box;
-    padding: 18px 8px 18px 8px;
+    padding: 16px 8px;
 
     .save {
       width: 8px;
       height: 8px;
-=======
-        > svg {
-            width: 18px;
-            height: 18px;
-            color: #FFFFFF;
-        }
->>>>>>> a773a4b843c509a0d50ff457efbdce53dfe2f17b
     }
 
     span {
       display: block;
       max-width: 210px;
-      height: 16px;
+      height: 25px;
       color: #ffffff;
       cursor: pointer;
       overflow: hidden;
@@ -282,62 +244,24 @@ onUnmounted(() => {
       font-size: 14px;
     }
 
-<<<<<<< HEAD
     input {
       display: block;
       max-width: 210px;
-      height: 16px;
+      height: 18px;
       border: none;
       outline: none;
       background-color: transparent;
       color: #FFFFFF;
       font-family: HarmonyOS Sans;
       font-size: 14px;
-=======
-    .rename {
-        max-width: 224px;
-        height: 52px;
-        flex: 1;
-        box-sizing: border-box;
-        padding: 16px 8px;
-
-        .save {
-            width: 8px;
-            height: 8px;
-        }
-
-        span {
-            display: block;
-            max-width: 210px;
-            height: 25px;
-            color: #ffffff;
-            cursor: pointer;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            white-space: nowrap;
-            font-family: HarmonyOS Sans;
-            font-size: 14px;
-        }
-
-        input {
-            display: block;
-            max-width: 210px;
-            height: 18px;
-            border: none;
-            outline: none;
-            background-color: transparent;
-            color: #FFFFFF;
-            font-family: HarmonyOS Sans;
-            font-size: 14px;
-        }
->>>>>>> a773a4b843c509a0d50ff457efbdce53dfe2f17b
     }
-  }
 
     .model {
-        flex: 0 0 72px;
-        display: flex;
-        align-items: center;
+      flex: 0 0 72px;
+      display: flex;
+      align-items: center;
     }
+
+  }
 }
 </style>
