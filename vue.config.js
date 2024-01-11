9f81cabc677f64d513db7f7513a1ad868a7bafed
Zrx <georgezrx@163.com>
2024-01-11 10:14:14 +0800

    fix: 0102

--- vue.config.js
+++ vue.config.js
@@ -121,8 +121,7 @@ var configureWebpack = (config) => {
 
 var exports = defineConfig({
     transpileDependencies: true,
-    // publicPath: './',
-    publicPath: '/zrx',
+    publicPath: './',
     configureWebpack,
 
     pluginOptions: {
