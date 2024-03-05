f4c9ca30e8bd77765e2ade11679c40237957b3ae
陈柱锋 <2414802324@qq.com>
2024-03-05 21:56:22 +0800

    namedColorMap 椭圆

--- src/utils/parse_svg.ts
+++ src/utils/parse_svg.ts
@@ -714,7 +714,161 @@ type MyColor = {
     a: number,
 }
 
+// https://developer.mozilla.org/zh-CN/docs/Web/CSS/named-color
+const namedColorMap: Record<string, string> = {
+    "black": "#000000",
+    "silver": "#c0c0c0",
+    "gray": "#808080",
+    "white": "#ffffff",
+    "maroon": "#800000",
+    "red": "#ff0000",
+    "purple": "#800080",
+    "fuchsia": "#ff00ff",
+    "green": "#008000",
+    "lime": "#00ff00",
+    "olive": "#808000",
+    "yellow": "#ffff00",
+    "navy": "#000080",
+    "blue": "#0000ff",
+    "teal": "#008080",
+    "aqua": "#00ffff",
+    "aliceblue": "#f0f8ff",
+    "antiquewhite": "#faebd7",
+    "aquamarine": "#7fffd4",
+    "azure": "#f0ffff",
+    "beige": "#f5f5dc",
+    "bisque": "#ffe4c4",
+    "blanchedalmond": "#ffebcd",
+    "blueviolet": "#8a2be2",
+    "brown": "#a52a2a",
+    "burlywood": "#deb887",
+    "cadetblue": "#5f9ea0",
+    "chartreuse": "#7fff00",
+    "chocolate": "#d2691e",
+    "coral": "#ff7f50",
+    "cornflowerblue": "#6495ed",
+    "cornsilk": "#fff8dc",
+    "crimson": "#dc143c",
+    "cyan": "#00ffff",
+    "darkblue": "#00008b",
+    "darkcyan": "#008b8b",
+    "darkgoldenrod": "#b8860b",
+    "darkgray": "#a9a9a9",
+    "darkgreen": "#006400",
+    "darkgrey": "#a9a9a9",
+    "darkkhaki": "#bdb76b",
+    "darkmagenta": "#8b008b",
+    "darkolivegreen": "#556b2f",
+    "darkorange": "#ff8c00",
+    "darkorchid": "#9932cc",
+    "darkred": "#8b0000",
+    "darksalmon": "#e9967a",
+    "darkseagreen": "#8fbc8f",
+    "darkslateblue": "#483d8b",
+    "darkslategray": "#2f4f4f",
+    "darkslategrey": "#2f4f4f",
+    "darkturquoise": "#00ced1",
+    "darkviolet": "#9400d3",
+    "deeppink": "#ff1493",
+    "deepskyblue": "#00bfff",
+    "dimgray": "#696969",
+    "dimgrey": "#696969",
+    "dodgerblue": "#1e90ff",
+    "firebrick": "#b22222",
+    "floralwhite": "#fffaf0",
+    "forestgreen": "#228b22",
+    "gainsboro": "#dcdcdc",
+    "ghostwhite": "#f8f8ff",
+    "gold": "#ffd700",
+    "goldenrod": "#daa520",
+    "greenyellow": "#adff2f",
+    "grey": "#808080",
+    "honeydew": "#f0fff0",
+    "hotpink": "#ff69b4",
+    "indianred": "#cd5c5c",
+    "indigo": "#4b0082",
+    "ivory": "#fffff0",
+    "khaki": "#f0e68c",
+    "lavender": "#e6e6fa",
+    "lavenderblush": "#fff0f5",
+    "lawngreen": "#7cfc00",
+    "lemonchiffon": "#fffacd",
+    "lightblue": "#add8e6",
+    "lightcoral": "#f08080",
+    "lightcyan": "#e0ffff",
+    "lightgoldenrodyellow": "#fafad2",
+    "lightgray": "#d3d3d3",
+    "lightgreen": "#90ee90",
+    "lightgrey": "#d3d3d3",
+    "lightpink": "#ffb6c1",
+    "lightsalmon": "#ffa07a",
+    "lightseagreen": "#20b2aa",
+    "lightskyblue": "#87cefa",
+    "lightslategray": "#778899",
+    "lightslategrey": "#778899",
+    "lightsteelblue": "#b0c4de",
+    "lightyellow": "#ffffe0",
+    "limegreen": "#32cd32",
+    "linen": "#faf0e6",
+    "magenta": "#ff00ff",
+    "mediumaquamarine": "#66cdaa",
+    "mediumblue": "#0000cd",
+    "mediumorchid": "#ba55d3",
+    "mediumpurple": "#9370db",
+    "mediumseagreen": "#3cb371",
+    "mediumslateblue": "#7b68ee",
+    "mediumspringgreen": "#00fa9a",
+    "mediumturquoise": "#48d1cc",
+    "mediumvioletred": "#c71585",
+    "midnightblue": "#191970",
+    "mintcream": "#f5fffa",
+    "mistyrose": "#ffe4e1",
+    "moccasin": "#ffe4b5",
+    "navajowhite": "#ffdead",
+    "oldlace": "#fdf5e6",
+    "olivedrab": "#6b8e23",
+    "orange": "#ffa500",
+    "orangered": "#ff4500",
+    "orchid": "#da70d6",
+    "palegoldenrod": "#eee8aa",
+    "palegreen": "#98fb98",
+    "paleturquoise": "#afeeee",
+    "palevioletred": "#db7093",
+    "papayawhip": "#ffefd5",
+    "peachpuff": "#ffdab9",
+    "peru": "#cd853f",
+    "pink": "#ffc0cb",
+    "plum": "#dda0dd",
+    "powderblue": "#b0e0e6",
+    "rebeccapurple": "#663399",
+    "rosybrown": "#bc8f8f",
+    "royalblue": "#4169e1",
+    "saddlebrown": "#8b4513",
+    "salmon": "#fa8072",
+    "sandybrown": "#f4a460",
+    "seagreen": "#2e8b57",
+    "seashell": "#fff5ee",
+    "sienna": "#a0522d",
+    "skyblue": "#87ceeb",
+    "slateblue": "#6a5acd",
+    "slategray": "#708090",
+    "slategrey": "#708090",
+    "snow": "#fffafa",
+    "springgreen": "#00ff7f",
+    "steelblue": "#4682b4",
+    "tan": "#d2b48c",
+    "thistle": "#d8bfd8",
+    "tomato": "#ff6347",
+    "transparent": "#00000000",
+    "turquoise": "#40e0d0",
+    "violet": "#ee82ee",
+    "wheat": "#f5deb3",
+    "whitesmoke": "#f5f5f5",
+    "yellowgreen": "#9acd32",
+}
+
 function parseColor(content: string): MyColor | undefined {
+    if (content in namedColorMap) content = namedColorMap[content];
     let color
     if (content.startsWith("rgba")) {
         const rgba = content.slice(5, -1).split(",").map(item => parseFloat(item))
@@ -786,10 +940,9 @@ type Attributes = { // 从元素的attributes中解析出来的属性
     stroke?: MyColor,
     strokeWidth?: number,
 
-    // 圆、椭圆
+    // 椭圆
     cx?: number,
     cy?: number,
-    r?: number,
     rx?: number,
     ry?: number,
 }
@@ -899,12 +1052,12 @@ class BaseShapeCreator implements ShapeCreator {
         if (cx) this.attributes.cx = parseFloat(cx);
         const cy = this.svgNode.getAttribute("cy")
         if (cy) this.attributes.cy = parseFloat(cy);
-        const r = this.svgNode.getAttribute("r")
-        if (r) this.attributes.r = parseFloat(r);
         const rx = this.svgNode.getAttribute("rx")
         if (rx) this.attributes.rx = parseFloat(rx);
         const ry = this.svgNode.getAttribute("ry")
         if (ry) this.attributes.ry = parseFloat(ry);
+        const r = this.svgNode.getAttribute("r")
+        if (r) this.attributes.rx = this.attributes.ry = parseFloat(r);
     }
 
     updateShapeAttrByTransform() { // 根据transform更新shape的属性
@@ -1115,6 +1268,23 @@ class RectShapeCreator extends BaseShapeCreator {
     }
 }
 
+class EllipseShapeCreator extends BaseShapeCreator {
+    create(): Shape | undefined {
+        const x = this.attributes.x || 0
+        const y = this.attributes.y || 0
+
+        let width = 0
+        if (this.attributes.rx) width = this.attributes.rx * 2;
+        else if (this.attributes.width) width = this.attributes.width;
+
+        let height = 0
+        if (this.attributes.ry) height = this.attributes.ry * 2;
+        else if (this.attributes.height) height = this.attributes.height;
+
+        return shapeCreator.newOvalShape("圆形", new ShapeFrame(x, y, width, height))
+    }
+}
+
 class Parser {
     svgRoot: Element
     context: any = {}
@@ -1134,6 +1304,8 @@ class Parser {
             creatorConstruction = PathShapeCreator
         } else if (node.tagName === "rect") {
             creatorConstruction = RectShapeCreator
+        } else if (node.tagName === "circle" || node.tagName === "ellipse") {
+            creatorConstruction = EllipseShapeCreator
         } else {
             creatorConstruction = NoneShapeCreator
         }
