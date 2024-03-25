77a0e021e95af8a8b34c1009d9e9aca6d2ffdd5e
Zrx <georgezrx@163.com>
2024-03-25 09:19:19 +0800

    修复剪切板问题

--- src/components/Document/ContentView.vue
+++ src/components/Document/ContentView.vue
@@ -623,7 +623,6 @@ onMounted(() => {
     props.context.tool.watch(tool_watcher);
     props.page.watch(page_watcher);
     props.context.color.watch(color_watcher);
-    // props.context.assist.init();
     props.context.user.updateUserConfig();
     rootRegister(true);
     updateBackground();
@@ -669,7 +668,7 @@ onUnmounted(() => {
 <template>
     <div :class="cursor" :data-area="rootId" ref="root" :reflush="reflush !== 0 ? reflush : undefined"
          @wheel="onMouseWheel" @mousedown="onMouseDown" @mousemove="onMouseMove_CV" @mouseleave="onMouseLeave"
-         @drop.prevent.stop="(e: DragEvent) => { drop(e, props.context, t as Function) }"
+         @drop.prevent.stop.capture.self="(e: DragEvent) => { drop(e, props.context, t as Function) }"
          @dragover.prevent.stop
          :style="{ 'background-color': background_color }">
         <PageViewVue :context="props.context" :data="(props.page as PageView)" :matrix="matrix"
