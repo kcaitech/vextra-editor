04b6789c67607015d343287e7d19f653834e0658
Zrx <georgezrx@163.com>
2024-04-05 12:51:26 +0800

    feat.像素网格

--- src/assets/lang/zh/setting.ts
+++ src/assets/lang/zh/setting.ts
@@ -1,6 +1,6 @@
 export const setting = {
     pixelAlignMentOn: '对齐像素已开启',
     pixelAlignMentOff: '对齐像素已关闭',
-    pixelGridOn: '像素网格已开启',
-    pixelGridOff: '像素网格已关闭',
+    pixelGridOn: '像素网格已显示',
+    pixelGridOff: '像素网格已隐藏',
 }
