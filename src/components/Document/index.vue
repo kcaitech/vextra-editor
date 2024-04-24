bf40710a85a3e8cdd0d6e29ebc5154101751a75b
陈柱锋 <2414802324@qq.com>
2024-04-24 09:25:39 +0800

    .

--- src/components/Document/index.vue
+++ src/components/Document/index.vue
@@ -319,7 +319,7 @@ const getUserInfo = async () => {
 }
 
 type UnwrappedPromise<T> = T extends Promise<infer U> ? U : T
-let documentLoader: UnwrappedPromise<ReturnType<typeof importRemote>>['loader'] | undefined = undefined;
+let documentLoader: UnwrappedPromise<ReturnType<typeof importDocument>>['loader'] | undefined = undefined;
 
 //获取文档信息
 const getDocumentInfo = async () => {
