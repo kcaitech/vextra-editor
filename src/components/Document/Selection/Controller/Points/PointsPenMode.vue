fdfda703defe91dd60c4bbbbcfe39a9fd1fe41c1
zrx <georgezrx@163.com>
2024-05-10 22:39:15 +0800

    【钢笔】断点处理

--- src/components/Document/Selection/Controller/Points/PointsPenMode.vue
+++ src/components/Document/Selection/Controller/Points/PointsPenMode.vue
@@ -29,27 +29,27 @@ const props = defineProps<Props>();
 const data: {
     dots: Dot[],
     segments: Segment[][]
-} = reactive({ dots: [], segments: [] });
-const { dots, segments } = data;
+} = reactive({dots: [], segments: []});
+const {dots, segments} = data;
 const dragActiveDis = 3;
 const new_high_light = ref<string>('');
 const add_rect = ref<string>('');
 let shape: ShapeView;
-let startPosition: ClientXY = { x: 0, y: 0 };
+let startPosition: ClientXY = {x: 0, y: 0};
 let isDragging = false;
 let bridged = false;
 
 let pathModifier: PathEditor | undefined;
-let downXY: XY = { x: 0, y: 0 };
+let downXY: XY = {x: 0, y: 0};
 
 let current_segment: number = -1;
 let current_curve_point_index: number = -1;
 
-const preXY = ref<XY>({ x: -10, y: -10 });
+const preXY = ref<XY>({x: -10, y: -10});
 
 const livingPathVisible = ref<boolean>(false);
 const livingPath = ref<string>('');
-const root = { ...props.context.workspace.root };
+const root = {...props.context.workspace.root};
 const maskPath = `M0 0, h${root.width} v${root.height} h${-root.width} z`;
 
 const preparePointVisible = ref<boolean>(false);
@@ -192,9 +192,9 @@ function point_mousedown(event: MouseEvent, segment: number, index: number) {
 
                 pathModifier = new PathEditor(props.context, event);
                 pathModifier.createApiCaller();
-                const __xy = { x: point.x, y: point.y };
+                const __xy = {x: point.x, y: point.y};
                 pathModifier.addPointForPen(last.segment, last.index + 1, preXY.value, __xy);
-                downXY = { x: event.x, y: event.y };
+                downXY = {x: event.x, y: event.y};
                 asyncEnvMount();
             }
         } else {
@@ -225,9 +225,9 @@ function point_mousedown(event: MouseEvent, segment: number, index: number) {
 
                 pathModifier = new PathEditor(props.context, event);
                 pathModifier.createApiCaller();
-                const __xy = { x: point.x, y: point.y };
+                const __xy = {x: point.x, y: point.y};
                 pathModifier.addPointForPen(last.segment, last.index + 1, preXY.value, __xy);
-                downXY = { x: event.x, y: event.y };
+                downXY = {x: event.x, y: event.y};
                 asyncEnvMount();
             }
         }
@@ -247,14 +247,14 @@ function point_mousedown(event: MouseEvent, segment: number, index: number) {
             const result = pathModifier.reverseSegment(segment);
 
             if (result) {
-                const { segment: _segment, activeIndex } = result;
+                const {segment: _segment, activeIndex} = result;
                 const point = (shape as PathShapeView)?.segments[_segment]?.points[activeIndex];
                 if (!point) {
                     return;
                 }
 
                 path.select_point(_segment, activeIndex);
-                path.setLastPoint({ point: point as CurvePoint, segment: _segment, index: activeIndex });
+                path.setLastPoint({point: point as CurvePoint, segment: _segment, index: activeIndex});
 
                 path.setContactStatus(true);
 
@@ -275,7 +275,7 @@ function point_mousedown(event: MouseEvent, segment: number, index: number) {
                 return;
             }
 
-            path.setLastPoint({ point: point as CurvePoint, segment, index })
+            path.setLastPoint({point: point as CurvePoint, segment, index})
             path.setContactStatus(true);
             path.select_point(segment, index);
 
@@ -314,7 +314,7 @@ function point_mousedown(event: MouseEvent, segment: number, index: number) {
                 return achieve;
             });
 
-            downXY = { x: event.x, y: event.y };
+            downXY = {x: event.x, y: event.y};
 
             asyncEnvMount();
         }
@@ -328,28 +328,21 @@ function checkStatus() {
         return;
     }
 
-    const { segment, index, handler, e } = params;
+    const {segment, index, handler, e} = params;
 
     current_segment = segment;
     current_curve_point_index = index;
-    downXY = { x: e.x, y: e.y };
+    downXY = {x: e.x, y: e.y};
 
     const point = (props.context.selection.selectedShapes[0] as PathShapeView)
         ?.segments[0]
         ?.points[0] as CurvePoint;
 
-    path.setLastPoint({ point, index: 0, segment: 0 });
+    path.setLastPoint({point, index: 0, segment: 0});
     path.select_point(segment, index);
 
     passiveUpdate();
 
-    if (!e.buttons) { // Mac环境与Window环境下，这个判断结果会不同，很奇怪的机制
-        handler?.fulfil();
-        return;
-    }
-
-    pathModifier = handler;
-    asyncEnvMount();
     const workspace = props.context.workspace;
 
     if (workspace.is_path_edit_mode) {
@@ -369,6 +362,13 @@ function checkStatus() {
         });
     }
 
+    if (!e.buttons) { // Mac环境与Window环境下，这个判断结果会不同，很奇怪的机制
+        handler?.fulfil();
+        return;
+    }
+
+    pathModifier = handler;
+    asyncEnvMount();
 }
 
 function point_mousemove(event: MouseEvent) {
@@ -385,8 +385,8 @@ function launch_bridging(event: MouseEvent) {
     if (!last || !pathModifier) {
         return;
     }
-    props.context.path.setBridgeParams({ handler: pathModifier, segment: last.segment, index: last.index, e: event });
-    props.context.path.bridging({ segment: -1, index: -1, event });
+    props.context.path.setBridgeParams({handler: pathModifier, segment: last.segment, index: last.index, e: event});
+    props.context.path.bridging({segment: -1, index: -1, event});
 
     pathModifier = undefined;
 
@@ -502,7 +502,7 @@ function documentMove(e: MouseEvent) {
     const xs = Array.from(mapX.keys());
     const ys = Array.from(mapY.keys());
 
-    const { x, y } = __client;
+    const {x, y} = __client;
 
     for (let j = 0; j < xs.length; j++) {
         const dx = xs[j] - x;
@@ -578,7 +578,7 @@ function modifyLivingPath() {
     }
 
     const shape = props.context.selection.selectedShapes[0] as PathShapeView;
-    const { segment, index } = path.lastPoint;
+    const {segment, index} = path.lastPoint;
     let previous = (shape as PathShapeView)?.segments[segment]?.points[index];
 
     if (!path.isContacting) return;
@@ -624,8 +624,8 @@ function down(e: MouseEvent) {
         if (lastPoint) {
             pathModifier = new PathEditor(props.context, e);
             pathModifier.createApiCaller();
-            pathModifier.addPointForPen(lastPoint.segment, lastPoint.index + 1, { ...preXY.value });
-            downXY = { x: e.x, y: e.y };
+            pathModifier.addPointForPen(lastPoint.segment, lastPoint.index + 1, {...preXY.value});
+            downXY = {x: e.x, y: e.y};
             asyncEnvMount();
 
             e.stopPropagation();
@@ -648,7 +648,7 @@ function down(e: MouseEvent) {
             return achieve;
         });
 
-        downXY = { x: e.x, y: e.y };
+        downXY = {x: e.x, y: e.y};
 
         asyncEnvMount();
 
@@ -673,7 +673,7 @@ function fixPreLine(e: MouseEvent, segmentIndex: number, toIndex: number) {
     }
 
     const shape = props.context.selection.selectedShapes[0] as PathShapeView;
-    const { segment, index } = path.lastPoint;
+    const {segment, index} = path.lastPoint;
     let previous = (shape as PathShapeView)?.segments[segment]?.points[index];
 
     if (!path.isContacting) return;
