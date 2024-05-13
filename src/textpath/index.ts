ee023af86b461fc0da0d6902f1de863bee73493a
Ruiqiang Zhang <zhangruiqiang@kcaitech.com>
2024-05-13 11:14:35 +0800

    fix 字符截断

--- src/textpath/index.ts
+++ src/textpath/index.ts
@@ -5,8 +5,9 @@ let __canvasElement: HTMLCanvasElement | undefined;
 
 export function getTextPath(font: string, fontSize: number, italic: boolean, weight: number, charCode: number): string {
 
-    const scale = 16;
-    fontSize = Math.round(fontSize * scale);
+    const saveFontSize = fontSize;
+    fontSize = Math.min(Math.round(fontSize * 16), 1024);// 最大1024可以了吧
+    const scale = fontSize / saveFontSize;
     const cacheId = font + "#" + fontSize + "#" + charCode + (italic ? '#i' : '') + '#' + weight;
     let path = __caches.get(cacheId);
     if (path) return path;
@@ -16,9 +17,9 @@ export function getTextPath(font: string, fontSize: number, italic: boolean, wei
     }
     const canvasElement: HTMLCanvasElement = __canvasElement;
 
-    const size = Math.round(fontSize * 1.15); // 预留防止截断，如italic，g字符
+    const size = Math.round(fontSize * 1.25); // 预留防止截断，如italic，g字符
     // const width = size; // italic截断
-    // const height = size; // 有字符如：g，底部被截断
+    // const height = size; // 有字符如：g，j底部被截断
     canvasElement.width = size;
     canvasElement.height = size;
     const canvas = canvasElement.getContext('2d')!
@@ -28,11 +29,13 @@ export function getTextPath(font: string, fontSize: number, italic: boolean, wei
     canvas.fillStyle = 'black'
     canvas.textAlign = 'left'
     canvas.textBaseline = 'alphabetic'
-    canvas.fillText(String.fromCharCode(charCode), 0, fontSize)
+
+    const offsetX = Math.round(fontSize * 0.1); // j左侧也会截断
+    canvas.fillText(String.fromCharCode(charCode), offsetX, fontSize)
 
     const imgdata: ImageData = canvas.getImageData(0, 0, size, size)
     const potrace = new Potrace(imgdata, () => { })
-    path = potrace.getSVGPath({ x: 1 / scale, y: 1 / scale }, { x: 0, y: ( - fontSize) / scale});//对齐哪里？
+    path = potrace.getSVGPath({ x: 1 / scale, y: 1 / scale }, { x: (- offsetX) / scale, y: (- fontSize) / scale });
     __caches.set(cacheId, path);
 
     return path;
