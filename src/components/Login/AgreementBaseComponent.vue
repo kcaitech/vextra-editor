89fbc21c4899f2c6d286784929ea8cea81492cc6
Zrx <georgezrx@163.com>
2024-05-16 18:22:49 +0800

    fix something

--- src/components/Login/AgreementBaseComponent.vue
+++ src/components/Login/AgreementBaseComponent.vue
@@ -1,118 +1,114 @@
 <template>
-    <div style="height: 100%;width: 100%;">
-    <div class="header-container">
-        <div class="kc-header">
-            <div class="logo" @click="router.go(-1)">
-                <img class="logo-img" :src=avatar alt="MossDesign">
+    <div class="container">
+        <div class="header-container">
+            <div class="kc-header">
+                <div class="logo" @click="router.go(-1)">
+                    <img class="logo-img" :src="avatar" alt="MossDesign">
+                </div>
+                <button class="loginbnt" type="button" @click.stop="router.push({ name: 'login' })">
+                    {{ t('system.btn_login') }}
+                </button>
             </div>
-            <button class="loginbnt"  type="button" @click.stop="router.push({ name: 'login' })">
-                {{ t('system.btn_login') }}
-            </button>
+        </div>
+        <div class="content-container">
+            <slot></slot>
+        </div>
+        <div class="footer-container">
+            <Footer/>
         </div>
     </div>
-    <!-- <div v-if="isMobile" class="phonetips"> {{ t('system.phonetips') }}</div> -->
-    <div class="content-container" :style="{ top: 56 + 'px' }">
-        <slot></slot>
-    </div>
-    <div class="footer-container" :style="{ top: 56 + 'px' }">
-        <Footer />
-    </div>
-</div>
 </template>
 <script setup lang="ts">
-import avatar from '@/assets/h-logo.svg'
+import avatar from '@/assets/h-logo3.svg'
 import { router } from '@/router'
 import Footer from './Footer.vue'
 import { useI18n } from 'vue-i18n'
-import { PROJECT_NAME } from "@/const";
 
 const { t } = useI18n();
 
 
 </script>
 <style lang="scss" scoped>
-.phonetips {
-    position: fixed;
-    font-size: 12px;
-    font-weight: 600;
-    line-height: 40px;
-    color: white;
-    top: 56px;
-    width: 100%;
-    text-align: center;
-    background-color: black;
-    z-index: 999;
-}
 
-.header-container {
-    width: 100%;
-    background-color: white;
-    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, .1);
+
+.container {
     display: flex;
-    justify-content: center;
-    position: fixed;
-    z-index: 999;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
 
-    .kc-header {
-        height: 56px;
+    .header-container {
+        flex: 0 0 56px;
         width: 100%;
-        max-width: 1200px;
+        background-color: white;
+        box-shadow: 0 2px 2px 0 rgba(0, 0, 0, .1);
         display: flex;
-        align-items: center;
-        justify-content: space-between;
+        justify-content: center;
+        position: sticky;
+        z-index: 999;
 
-        .logo {
+        .kc-header {
+            height: 56px;
+            width: 100%;
+            max-width: 1200px;
             display: flex;
             align-items: center;
-            cursor: pointer;
+            justify-content: space-between;
 
-            .logo-img {
-                width: 200px;
-                height: 56px;
+            .logo {
+                display: flex;
+                align-items: center;
+                cursor: pointer;
+
+                .logo-img {
+                    width: 200px;
+                    height: 56px;
+                }
+
+                .logo-text {
+                    font-size: 24px;
+                    font-weight: 700;
+                }
             }
 
-            .logo-text {
-                font-size: 24px;
-                font-weight: 700;
-            }
-        }
+            .loginbnt {
+                height: 40px;
+                width: 80px;
+                border: 1px solid black;
+                border-radius: 4px;
+                background-color: white;
+                cursor: pointer;
 
-        .loginbnt {
-            height: 40px;
-            width: 80px;
-            border: 1px solid black;
-            border-radius: 4px;
-            background-color: white;
-            cursor: pointer;
-
-            &:hover {
-                border: 1px solid #1878F5;
-                color: #1878F5;
+                &:hover {
+                    border: 1px solid #1878F5;
+                    color: #1878F5;
+                }
             }
         }
     }
-}
 
-.content-container {
-    position: relative;
-    top: 56px;
-    display: flex;
-    justify-content: center;
-}
-
-
-.footer-container {
-    width: 100%;
-    display: flex;
-    align-items: flex-end;
-    justify-content: center;
-    height: 56px;
-    position: relative;
-    top: 56px;
-    // background-color: #9775fa;
-
-    .footer {
+    .content-container {
+        flex: 1;
         position: relative;
+        display: flex;
+        justify-content: center;
+        overflow: auto;
+
+    }
+
+    .footer-container {
+        flex: 0 0 56px;
+        width: 100%;
+        display: flex;
+        align-items: flex-end;
+        justify-content: center;
+        height: 56px;
+        position: relative;
+
+        .footer {
+            position: relative;
+        }
     }
 }
+
 </style>
