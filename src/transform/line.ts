0aae6feb817582920e234e3529b110cbce27fe47
Zrx <georgezrx@163.com>
2024-05-22 10:13:13 +0800

    【直线交互】直线固定角度处理

--- src/transform/line.ts
+++ src/transform/line.ts
@@ -4,6 +4,7 @@ import { XY } from "@/context/selection";
 import { Context } from "@/context";
 import { Assist } from "@/context/assist";
 import { getHorizontalAngle } from "@/utils/common";
+import { fixed } from "@/textpath/utils";
 
 export function round2half(f: number) {
     if (f === 0) return 0;
@@ -162,13 +163,9 @@ export class LineHandler extends TransformHandler {
         const _xy = this.inverse.computeCoord2(x, y);
 
         if (this.ctrlElement === CtrlElementType.LineStart) {
-            const end = this.lineShape.segments[0].points[1];
-            if (!end) return;
-            (this.asyncApiCaller as LineHandleApiCaller).execute(_xy, end);
+            this.__execute4lineStart();
         } else if (this.ctrlElement === CtrlElementType.LineEnd) {
-            const start = this.lineShape.segments[0].points[0];
-            if (!start) return;
-            (this.asyncApiCaller as LineHandleApiCaller).execute(start, _xy);
+            this.__execute4lineEnd();
         } else if (this.ctrlElement === CtrlElementType.LineStartR) {
             this.getAngle();
         } else if (this.ctrlElement === CtrlElementType.LineEndR) {
@@ -176,5 +173,120 @@ export class LineHandler extends TransformHandler {
         }
 
         this.updateCtrlView();
+    }
+
+    private __execute4lineStart() {
+        const __end = this.lineShape.segments[0].points[1];
+        if (!__end) return;
+
+        const end = { x: __end.x, y: __end.y };
+
+        let x, y;
+
+        if (this.alignPixel) {
+            x = round2half(this.livingPoint.x);
+            y = round2half(this.livingPoint.y);
+        } else {
+            x = this.livingPoint.x;
+            y = this.livingPoint.y;
+        }
+
+        const start = this.inverse.computeCoord2(x, y);
+
+        if (this.shiftStatus) {
+            this.fixTargetPointFromFixedRotation(end, start);
+        }
+
+        (this.asyncApiCaller as LineHandleApiCaller).execute(start, end);
+    }
+
+    private __execute4lineEnd() {
+        const __start = this.lineShape.segments[0].points[0];
+        if (!__start) return;
+
+        const start = { x: __start.x, y: __start.y };
+
+        let x, y;
+
+        if (this.alignPixel) {
+            x = round2half(this.livingPoint.x);
+            y = round2half(this.livingPoint.y);
+        } else {
+            x = this.livingPoint.x;
+            y = this.livingPoint.y;
+        }
+
+        const end = this.inverse.computeCoord2(x, y);
+
+        if (this.shiftStatus) {
+            this.fixTargetPointFromFixedRotation(start, end);
+        }
+
+        (this.asyncApiCaller as LineHandleApiCaller).execute(start, end);
+    }
+
+    private fixTargetPointFromFixedRotation(_fixed: XY, _living: XY) {
+        const fixed = this.matrix.computeCoord3(_fixed);
+        const living = this.matrix.computeCoord3(_living);
+
+        const angle = getHorizontalAngle(fixed, living);
+
+        if (angle >= 0 && angle < 22.5) {
+            living.y = fixed.y;
+        } else if (angle >= 22.5 && angle < 67.5) {
+            const a = angle / 180;
+            const __m = new Matrix();
+            __m.rotate((0.25 - a) * Math.PI, fixed.x, fixed.y);
+            const __living = __m.computeCoord3(living);
+
+            living.x = __living.x;
+            living.y = __living.y;
+        } else if (angle >= 67.5 && angle < 112.5) {
+            living.x = fixed.x;
+        } else if (angle >= 112.5 && angle < 157.5) {
+            const a = angle / 180;
+            const __m = new Matrix();
+            __m.rotate((0.75 - a) * Math.PI, fixed.x, fixed.y);
+            const __living = __m.computeCoord3(living);
+
+            living.x = __living.x;
+            living.y = __living.y;
+        } else if (angle >= 157.5 && angle < 202.5) {
+            living.y = fixed.y;
+        } else if (angle >= 202.5 && angle < 247.5) {
+            const a = angle / 180;
+            const __m = new Matrix();
+            __m.rotate((1.25 - a) * Math.PI, fixed.x, fixed.y);
+            const __living = __m.computeCoord3(living);
+
+            living.x = __living.x;
+            living.y = __living.y;
+        } else if (angle >= 247.5 && angle < 292.5) {
+            const a = angle / 180;
+            const __m = new Matrix();
+            __m.rotate((1.5 - a) * Math.PI, fixed.x, fixed.y);
+            const __living = __m.computeCoord3(living);
+
+            living.x = __living.x;
+            living.y = __living.y;
+        } else if (angle >= 292.5 && angle < 337.5) {
+            const a = angle / 180;
+            const __m = new Matrix();
+            __m.rotate((1.75 - a) * Math.PI, fixed.x, fixed.y);
+            const __living = __m.computeCoord3(living);
+
+            living.x = __living.x;
+            living.y = __living.y;
+        } else {
+            living.y = fixed.y;
+        }
+
+        const __f = this.inverse.computeCoord3(fixed);
+        const __l = this.inverse.computeCoord3(living);
+
+        _fixed.x = __f.x;
+        _fixed.y = __f.y;
+        _living.x = __l.x;
+        _living.y = __l.y;
     }
 }
