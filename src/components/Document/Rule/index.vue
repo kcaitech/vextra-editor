e2c1c06c517fc2ba88cc44fe20c230ed31923160
zrx <georgezrx@163.com>
2024-05-23 23:45:49 +0800

    【Rule】标尺基础

--- src/components/Document/Rule/index.vue
+++ src/components/Document/Rule/index.vue
@@ -4,20 +4,89 @@ import { Context } from "@/context";
 import { onMounted, onUnmounted, ref } from "vue";
 import { User } from "@/context/user";
 import { WorkSpace } from "@/context/workspace";
+import { Matrix } from '@kcdesign/data';
 
 const props = defineProps<{
     context: Context
 }>();
 
+interface Scale {
+    offset: number;
+    data: number;
+    opacity: number;
+
+}
+
 const ruleVisible = ref<boolean>(false);
+const scalesHor = ref<Scale[]>([])
 
 function render() {
+    scalesHor.value.length = 0;
     ruleVisible.value = props.context.user.isRuleVisible;
 
     if (!ruleVisible.value) {
         return;
     }
 
+    const matrix = new Matrix(props.context.workspace.matrix);
+
+    const percent = Math.round(matrix.m00 * 100);
+
+    matrix.trans(-20, -20);
+
+    const inverse = new Matrix(matrix.inverse);
+    const {width, height} = props.context.workspace.root;
+
+    const scale = getScale(percent);
+
+    let startX = inverse.computeCoord2(0, 0).x;
+    let endX = inverse.computeCoord2(width + 20, 0).x;
+
+    startX -= startX % scale;
+    endX += scale - endX % scale;
+
+    for (let x = startX; x < endX; x += scale) {
+        const offset = matrix.computeCoord2(x, 0).x - 24.5;
+
+        let scale: Scale = {data: x, opacity: getOpacity(offset), offset};
+        scalesHor.value.push(scale);
+    }
+}
+
+function getScale(percent: number) {
+    let scale;
+    if (percent >= 2 && percent < 5) {
+        scale = 2500;
+    } else if (percent >= 5 && percent < 10) {
+        scale = 1000;
+    } else if (percent >= 10 && percent < 20) {
+        scale = 500;
+    } else if (percent >= 20 && percent < 50) {
+        scale = 250;
+    } else if (percent >= 50 && percent < 100) {
+        scale = 100;
+    } else if (percent >= 100 && percent < 200) {
+        scale = 50;
+    } else if (percent >= 200 && percent < 500) {
+        scale = 25;
+    } else if (percent >= 500 && percent < 1000) {
+        scale = 10
+    } else if (percent >= 1000 && percent < 2500) {
+        scale = 5;
+    } else if (percent >= 2500 && percent < 5000) {
+        scale = 2;
+    } else {
+        scale = 1;
+    }
+    return scale;
+}
+
+function getOpacity(distance: number) {
+    if (distance > 10) {
+        return 1;
+    } else {
+        return Number(((distance + 30) / 40).toFixed(1));
+    }
 }
 
 function workspaceWatcher(t: number) {
@@ -44,7 +113,18 @@ onUnmounted(() => {
 <template>
     <div v-if="ruleVisible" class="rule-container">
         <div class="contact-block"></div>
-        <div class="d-hor"></div>
+        <div class="d-hor">
+            <div v-for="(s, i) in scalesHor"
+                 :key="i"
+                 :style="{transform: `translateX(${s.offset}px)`, opacity: s.opacity}"
+                 class="scale"
+            >
+                <div class="scale-number">
+                    {{ s.data }}
+                </div>
+                <div class="dot"></div>
+            </div>
+        </div>
         <div class="d-ver"></div>
     </div>
 </template>
@@ -73,6 +153,28 @@ onUnmounted(() => {
         box-sizing: border-box;
         border-bottom: 1px solid var(--grey);
         background-color: var(--theme-color-anti);
+        overflow: hidden;
+
+        > .scale {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 50px;
+            height: 100%;
+            font-size: 10px;
+            color: grey;
+
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: space-between;
+        }
+
+        .dot {
+            width: 1px;
+            height: 4px;
+            background-color: grey;
+        }
     }
 
     .d-ver {
