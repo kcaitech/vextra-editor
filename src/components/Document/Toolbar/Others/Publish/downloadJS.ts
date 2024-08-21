a43daa70ba277a2884205680d9c3800ab6fe7c89
Zrx <georgezrx@163.com>
2024-08-21 11:49:18 +0800

    【FEAT】refactor download file structure

--- src/components/Document/Toolbar/Others/Publish/downloadJS.ts
+++ src/components/Document/Toolbar/Others/Publish/downloadJS.ts
@@ -21,63 +21,82 @@ export class MossPacker {
 
     }
 
-    async insertImage(refs: string[], folder: JSZip) {
-        if (!refs.length) return;
-        const manage = this.m_context.data.mediasMgr;
-        for (const ref of refs) {
-            const media = await manage.get(ref);
-            if (!media) continue;
-            folder.file(ref, media.buff);
-        }
-    }
 
     async pack(config: any) {
+        const doc = this.m_doc;
+        const createName = this.createDocName;
+
+        const data = await exportExForm(doc)
+            .catch((error) => {
+                throw error
+            });
+        console.log('__data__', data);
+        if (!data) throw new Error('invalid data');
+
         try {
-            const doc = this.m_doc;
-            const createName = this.createDocName;
-
-            const data = await exportExForm(doc)
-                .catch((error) => {
-                    throw error
-                });
-            console.log('__data__', data);
-            if (!data) throw new Error('invalid data');
-
             const zip = new JSZip();
-            const main = zip.folder('');
-            if (!main) throw new Error('wrong main folder');
 
-            // 配置文件
-            const configBlob = new Blob([JSON.stringify(config)], { type: 'config' });
-            main.file('.config', configBlob);
+            const readmeBlob = generateReadme();
+            zip.file('README.md', readmeBlob);
 
-            // .mdd
-            const zipMDD = new JSZip();
-            const mddFolder = zipMDD.folder('');
-            if (!mddFolder) throw new Error('wrong mdd folder');
-            // doc.json
-            const dataBlob = new Blob([JSON.stringify(data)], { type: 'json' });
-            mddFolder.file(createName(doc.name, 'json'), dataBlob);
-            // 静态资源
-            const imageFolder = mddFolder.folder('assets');
-            if (imageFolder) await this.insertImage(data.media_names, imageFolder);
-            const contentMDD = await zipMDD.generateAsync({ type: 'blob' });
-            main.file(createName(doc.name, 'mdd'), contentMDD);
+            const web = zip.folder('web')!;
 
-            // index.html
-            main.file('index.html', template);
+            const html = generateIndexHTML();
+            web.file('index.html', html);
 
-            // README.md
-            main.file('README.md', readme);
+            const _static = web.folder('static')!;
+            const config = generateConfig();
+            _static.file('.config', config);
+            const doc = generateDoc();
+            _static.file('document-meta.json', doc);
+            generatePages(_static);
+            const images = _static.folder('images')!;
+            await generateImages(images, this.m_context);
 
             const content = await zip.generateAsync({ type: 'blob' });
 
             const link = document.createElement('a');
             link.href = URL.createObjectURL(content);
-            link.download = 'download.zip';
+            link.download = createName(data.document_meta.name) + '.zip';
             link.click();
         } catch (e) {
             console.error(e);
+        }
+
+        function generateReadme() {
+            return readme;
+        }
+
+        function generateIndexHTML() {
+            return template;
+        }
+
+        function generateIndexJS() {
+            return; // todo index.js
+        }
+
+        function generateConfig() {
+            return new Blob([JSON.stringify(config)], { type: 'config' });
+        }
+
+        function generateDoc() {
+            return new Blob([JSON.stringify(data.document_meta)], { type: 'json' });
+        }
+
+        function generatePages(folder: JSZip) {
+            data.pages.forEach(p => {
+                folder.file(p.id + '.json', new Blob([JSON.stringify(p)]));
+            });
+        }
+
+        async function generateImages(folder: JSZip, context: Context) {
+            if (!data.media_names.length) return;
+            const manager = context.data.mediasMgr;
+            for (const ref of data.media_names) {
+                const media = await manager.get(ref);
+                if (!media) continue;
+                folder.file(ref, media.buff);
+            }
         }
     }
 }
