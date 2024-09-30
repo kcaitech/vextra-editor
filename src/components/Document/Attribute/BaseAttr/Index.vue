e7b3bd9bbe259a93b0b5499060dbc7bbb24ce057
Kc_tjs <3288771283@qq.com>
2024-09-30 09:31:11 +0800

    整理限制

--- src/components/Document/Attribute/BaseAttr/Index.vue
+++ src/components/Document/Attribute/BaseAttr/Index.vue
@@ -760,7 +760,7 @@ const _whetherTidyUp = () => {
     const selected = props.context.selection.selectedShapes;
     s_tidy_up.value = false;
     const length = selected.filter(shape => shape.isVisible).length;
-    if (length <= 1) return;
+    if (length <= 1 || length > 100) return;
     s_tidy_up.value = true;
     if (props.context.workspace.isTranslating || props.context.workspace.isScaling || props.context.workspace.isRotating) return;
     const Info = whetherNeedTidyUp(props.context);
