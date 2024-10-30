bc882e8160fc2359b1bc5ef08fc72f18f9264367
Zrx <georgezrx@163.com>
2024-10-30 09:38:12 +0800

    feat.write

--- src/clipboard/index.ts
+++ src/clipboard/index.ts
@@ -1,6 +1,19 @@
 import { Context } from "@/context";
-import { CurvePoint, export_text, import_text, TableCellType, Text, TransformRaw } from "@kcdesign/data";
+import {
+    adapt2Shape,
+    ContactLineView,
+    ContactShape,
+    CurvePoint, Document, export_shape,
+    export_text,
+    import_text,
+    makeShapeTransform1By2, PathShape, ShapeType,
+    TableCellType,
+    Text,
+    TransformRaw
+} from "@kcdesign/data";
 import { compare_layer_3 } from "@/utils/group_ungroup";
+import { v4 } from "uuid";
+import { identity } from "@/utils/clipboard";
 
 interface Bundle {
     HTML?: string;
@@ -11,6 +24,11 @@ interface Bundle {
         height?: number;
         width?: number;
     }
+}
+
+class ExfContext {
+    symbols = new Set<string>()
+    medias = new Set<string>()
 }
 export class Clipboard {
     static identity = 'design.moss';
@@ -73,6 +91,16 @@ export class Clipboard {
             return t;
         }
     }
+
+    private __sort_media(document: Document, exportCtx: ExfContext) {
+        const media: any = {};
+        exportCtx.medias.forEach(v => {
+            const res = document.mediasMgr.getSync(v)?.base64;
+            if (!res) return;
+            media[v] = res;
+        });
+        return media;
+    }
     /**
      * @description 先读同步，再读异步
      */
@@ -122,12 +150,12 @@ export class Clipboard {
 
     async write(event?: ClipboardEvent) {
         const text = this.__text;
+        const cache: Bundle = {};
         if (text) {
             const _text = export_text(text);
             const plain_text = text.getText(0, text.length);
             const html = this.encode(Clipboard.paras, _text, plain_text);
-            let is_async_plan_enable = false;
-            // @ts-ignore
+            let is_async_plan_enable;
             const text_html = new Blob([html || ''], { type: 'text/html' });
             const text_plain = new Blob([plain_text], { type: 'text/plain' });
             const content = [new ClipboardItem({ "text/plain": text_plain, 'text/html': text_html })];
@@ -142,16 +170,60 @@ export class Clipboard {
             event.clipboardData.setData('text/html', html);
             event.preventDefault();
 
-            return true;
+            cache["HTML"] = html;
+            cache["plain"] = plain_text;
         } else {
-            let shapes = compare_layer_3(this.context.selection.selectedShapes, -1); // 处理层级
-
+            let shapes = compare_layer_3(this.context.selection.selectedShapes, -1);
             if (!shapes.length) return false;
-
             const origin_transform_map: any = {};
             const position_map: Map<string, TransformRaw> = new Map();
             const points_map: Map<string, CurvePoint[]> = new Map();
+
+            for (let i = 0, len = shapes.length; i < len; i++) {
+                const shape = shapes[i];
+                origin_transform_map[`${shape.id}`] = shape.transform.clone();
+                position_map.set(shape.id, makeShapeTransform1By2(shape.transform2FromRoot) as TransformRaw);
+                if (shape instanceof ContactLineView) points_map.set(shape.id, shape.getPoints());
+            }
+
+            const { shapes: _shapes, ctx } = export_shape(shapes.map((s => adapt2Shape(s))));
+            if (!_shapes) return false;
+
+            for (let i = 0, len = _shapes.length; i < len; i++) {
+                const shape = _shapes[i];
+                shape.transform = position_map.get(shape.id)!;
+                const points = points_map.get(shape.id);
+                if (points) {
+                    (shape as PathShape).pathsegs[0].points = points.map(i => new CurvePoint(i.crdtidx, v4(), i.x, i.y, i.mode)) as any;
+                }
+            }
+
+            const media = this.__sort_media(this.context.data, ctx);
+            const data = {
+                originTransform: origin_transform_map,
+                shapes: _shapes,
+                media,
+            }
+
+            const html = this.encode(identity, data);
+
+            let is_async_plan_enable;
+
+            const blob = new Blob([html || ''], { type: 'text/html' });
+            const item: any = { 'text/html': blob };
+
+            await navigator.clipboard.write([new ClipboardItem(item)]);
+
+            is_async_plan_enable = true;
+
+            if (!event?.clipboardData) return is_async_plan_enable;
+
+            event.clipboardData.clearData();
+            event.clipboardData.setData('text/html', html);
+            event.preventDefault();
+            cache["HTML"] = html;
         }
+        return true;
     }
 
     init() {
