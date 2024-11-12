c55fb17d66486be93ff1134b1e437492aea24eeb
Zrx <georgezrx@163.com>
2024-11-12 16:21:03 +0800

    fix.shape4blur

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -475,7 +475,7 @@ export function modifyOpacity(context: Context, val: number, _shapes?: ShapeView
     const page = context.selection.selectedPage!;
     const shapes = _shapes || context.selection.selectedShapes;
     const editor = context.editor4Page(page);
-    editor.modifyShapesContextSettingOpacity((shapes as ShapeView[]).map(s => adapt2Shape(s)), val);
+    editor.modifyShapesContextSettingOpacity(shapes, val);
     hidden_selection(context);
 }
 
