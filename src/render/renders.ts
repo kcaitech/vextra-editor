c32e3583ee8a2a2df358f991ee171a3b1d72b7e4
zhangruiqiang <zhangruiqiang@kcaitech.com>
2024-12-03 18:07:40 +0800

    canvasapi

--- src/render/renders.ts
+++ src/render/renders.ts
@@ -1,18 +1,14 @@
 // 多线程绘制
 
 import { CanvasCache } from "./canvascache"
+import { Memory } from "./memory"
 import { ThreadPool } from "./threadpool"
 
-
-
-
-
 const thread_count = Math.min(4, Math.max(navigator.hardwareConcurrency, 2))
-class Render {
+export class Render {
     private canvas: CanvasCache = new CanvasCache(thread_count * 2, 15000/* 15s */)
     private threads: ThreadPool = new ThreadPool(thread_count)
-
-    // 是否需要？
+    private memory: Memory = new Memory()
 
 
 
