6660ce5d8f8de832f258b7622d8d0b463af2fb85
zrx <georgezrx@163.com>
2024-12-06 00:10:58 +0800

    feat.clip path for shift

--- src/path/clipper.ts
+++ src/path/clipper.ts
@@ -2,6 +2,7 @@ import { PathShapeView, CurvePoint } from "@kcdesign/data";
 import { Context } from "@/context";
 
 interface ClipResult {
+    closed: boolean;
     originSegmentIndex: number;
     slices: CurvePoint[][];
 }
@@ -22,7 +23,7 @@ export class PathClipper {
         if (sides.length === 1 && closed) { // 打开路径就好了
             const index = sides[0];
             slices.push([...points.slice(index + 1), ...points.slice(0, index + 1)]);
-            return;
+            return closed;
         }
         points = points.slice(0);
         sides = sides.sort((a, b) => b - a);
@@ -52,6 +53,8 @@ export class PathClipper {
             points = [...points.slice(last + 1), ...points.slice(0, last + 1)];
         }
         slices.unshift(points);
+
+        return closed;
     }
 
     private del(slices: CurvePoint[][], indexes: number[], points: CurvePoint[]) {
@@ -66,7 +69,38 @@ export class PathClipper {
         }
     }
 
-    private __clip() {
+    private cut(slices: CurvePoint[][], indexes: number[], points: CurvePoint[], isClosed: boolean) {
+        const idSet = new Set<string>();
+        for (const index of indexes) idSet.add(points[index].id);
+        if (!slices.length) slices.push(points.slice(0));
+        const __slices: CurvePoint[][] = [];
+        for (let j = slices.length - 1; j > -1; j--) {
+            let slice = slices[j];
+            let copy = slice.slice(0);
+            let lastIdx = slice.length - 1;
+            let left: CurvePoint[] = [];
+            for (let i = slice.length - 1; i > -1; i--) {
+                if (!idSet.has(slice[i].id)) continue;
+                if (isClosed) {
+                    left = copy.slice(i + 1);
+                    copy = copy.slice(0, i);
+                    lastIdx = i;
+                    isClosed = false;
+                } else {
+                    __slices.unshift(copy.slice(i + 1, lastIdx + 1));
+                    copy = copy.slice(0, i);
+                    lastIdx = i;
+                }
+            }
+            if (left.length) copy = [...left, ...copy];
+            __slices.unshift(copy);
+        }
+        slices.length = 0;
+        slices.push(...__slices);
+        return closed;
+    }
+
+    private __clip(keepClosed = false) {
         const path = this.context.path;
         const segments = this.view.segments;
         const sides = path.selectedSides;
@@ -87,16 +121,23 @@ export class PathClipper {
             const segment = segments[index];
             const slices: CurvePoint[][] = [];
             const _sides = sides.get(index);
-            if (_sides) this.disconnect(slices, _sides, segment.points as CurvePoint[], segment.isClosed);
+            let closed = segment.isClosed;
+            if (_sides) closed = this.disconnect(slices, _sides, segment.points as CurvePoint[], segment.isClosed);
             const _points = points.get(index);
-            if (_points) this.del(slices, _points, segment.points as CurvePoint[]);
-            result.push({originSegmentIndex: index, slices: slices.filter(s => s.length > 1)});
+            if (_points) {
+                if (keepClosed) {
+                    this.del(slices, _points, segment.points as CurvePoint[]);
+                } else {
+                    closed = this.cut(slices, _points, segment.points as CurvePoint[], segment.isClosed && !_sides);
+                }
+            }
+            result.push({closed, originSegmentIndex: index, slices: slices.filter(s => s.length > 1)});
         }
         return result;
     }
 
-    clip(keepClosed = true) {
-        const params = this.__clip();
+    clip(keepClosed = false) {
+        const params = this.__clip(keepClosed);
         if (!params.length) return -1;
         return this.context.editor4Shape(this.view).clipPath(params)
     }
