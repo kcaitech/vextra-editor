dbb316e34802021ae4add7ba183dd102434e7ab8
Zrx <kcai@kcai.com>
2025-01-21 16:02:25 +0800

    code review

--- src/components/common/draggable.ts
+++ src/components/common/draggable.ts
@@ -4,16 +4,16 @@ import { XY } from "@/context/selection";
  * @description 主键拖拽集成工具
  */
 export class DragKit {
-    private m_down: Function;
-    private m_move: Function;
-    private m_commit: Function;
-    private m_stop: boolean;
+    private readonly m_down: Function;
+    private readonly m_move: Function;
+    private readonly m_commit: Function;
+    private readonly m_stop: boolean;
 
     constructor(options: {
         down?: Function, /* 注意这几个函数的this指向 */
         move?: Function,
         commit?: Function,
-        stop?: boolean
+        stop?: boolean  /* 是否阻止事件冒泡，默认为true */
     }) {
         this.m_down = options.down ?? (() => {});
         this.m_move = options.move ?? (() => {});
@@ -52,7 +52,7 @@ export class DragKit {
     private m_origin_xy: XY;
     private m_down_xy: XY;
 
-    // params 中的xy接收start执行时某一变量[refVal]在对应维度的值。这个值会在move中，加上clientXY的delta值作为参数传递出去，可用来更新[refVal]。
+    // params 中的xy接收start执行时某一变量[refVal]在对应维度的值。这个值会在move中加上clientXY的delta值作为参数传递出去，可用来更新[refVal]。
     start(event: MouseEvent, params?: { x?: number, y?: number }) {
         if (event.button) return;
         this.m_origin_xy = {x: params?.x ?? event.clientX, y: params?.y ?? event.clientY};
