855f618e3a272c6de73a3e65f83f59003d383c6c
Kc_tjs <3288771283@qq.com>
2025-02-19 16:10:44 +0800

    update

--- src/components/Document/Selection/Controller/ImageEdit/Tile.vue
+++ src/components/Document/Selection/Controller/ImageEdit/Tile.vue
@@ -5,7 +5,7 @@ import { ClientXY } from '@/context/selection';
 import { WorkSpace } from '@/context/workspace';
 import { getHorizontalAngle } from '@/utils/common';
 import { CursorType } from '@/utils/cursor2';
-import { ColorPicker, ColVector3D, CtrlElementType, Matrix, ShapeView, TransformRaw } from '@kcdesign/data';
+import { ColorPicker, ColVector3D, CtrlElementType, makeShapeTransform2By1, Matrix, ShapeView, TransformRaw } from '@kcdesign/data';
 import { onMounted, onUnmounted, reactive, ref } from 'vue';
 
 interface Props {
@@ -13,7 +13,7 @@ interface Props {
     matrix: Matrix;
 }
 
-enum Direction {X, Y, Angle}
+enum Direction { X, Y, Angle }
 
 const props = defineProps<Props>();
 const image_width = ref(0);
@@ -139,15 +139,23 @@ function getVectors() {
 
     const { x, y, width, height } = shape.frame;
 
-    const clientMatrix = (props.context.workspace.matrix);
-    const fromRoot = shape.matrix2Root();
-    const fromClient = fromRoot.multi(clientMatrix);
-    return fromClient.transform([
+    const clientMatrix = makeShapeTransform2By1(props.context.workspace.matrix);
+    const fromRoot = makeShapeTransform2By1(shape.matrix2Root());
+
+    const fromClient = fromRoot.addTransform(clientMatrix);
+
+    const {
+        col0: vecLT,
+        col1: vecRT,
+        col2: vecRB,
+        col3: vecLB
+    } = fromClient.transform([
         ColVector3D.FromXY(x, y),
         ColVector3D.FromXY(x + width, y),
         ColVector3D.FromXY(x + width, y + height),
         ColVector3D.FromXY(x, y + height),
     ]);
+    return [vecLT, vecRT, vecRB, vecLB];
 }
 
 function setCursor(t: CtrlElementType) {
@@ -243,57 +251,54 @@ onUnmounted(() => {
 </script>
 
 <template>
-<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" overflow="visible" :width="100"
-     :height="100" viewBox="0 0 100 100" style="position: absolute;">
-    <g :style="{ transform: transform }">
-        <rect x="0" y="0" :width="image_width" :height="image_height" fill="rgba(0,0,0,0.35)"></rect>
-        <g v-if="Math.max(image_width, image_height) > 20">
-            <!-- 左上 -->
-            <path :d="`M 16 2.5 L 2.5 2.5 L 2.5 16`" stroke="#1878f5" stroke-width="5" fill="none"></path>
-            <!-- 中上 -->
-            <path :d="`M ${(image_width / 2) - 8} 2.5 L ${(image_width / 2) + 8} 2.5`" stroke="#1878f5"
-                  stroke-width="5" fill="none"></path>
-            <!-- 右上 -->
-            <path :d="`M ${image_width - 16} 2.5 L ${image_width - 2.5} 2.5 L ${image_width - 2.5} 16`"
-                  stroke="#1878f5" stroke-width="5" fill="none"></path>
-            <!-- 中左 -->
-            <path :d="`M 2.5 ${(image_height / 2) - 8} L 2.5 ${(image_height / 2) + 8}`" stroke="#1878f5"
-                  stroke-width="5" fill="none"></path>
-            <!-- 中右 -->
-            <path
-                :d="`M ${image_width - 2.5} ${(image_height / 2) - 8} L ${image_width - 2.5} ${(image_height / 2) + 8}`"
-                stroke="#1878f5" stroke-width="5" fill="none"></path>
-            <!-- 左下 -->
-            <path :d="`M 2.5 ${image_height - 16} L 2.5 ${image_height - 2.5} L 16 ${image_height - 2.5}`"
-                  stroke="#1878f5" stroke-width="5" fill="none"></path>
+    <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" overflow="visible" :width="100"
+        :height="100" viewBox="0 0 100 100" style="position: absolute;">
+        <g :style="{ transform: transform }">
+            <rect x="0" y="0" :width="image_width" :height="image_height" fill="rgba(0,0,0,0.35)"></rect>
+            <g v-if="Math.max(image_width, image_height) > 20">
+                <!-- 左上 -->
+                <path :d="`M 16 2.5 L 2.5 2.5 L 2.5 16`" stroke="#1878f5" stroke-width="5" fill="none"></path>
+                <!-- 中上 -->
+                <path :d="`M ${(image_width / 2) - 8} 2.5 L ${(image_width / 2) + 8} 2.5`" stroke="#1878f5"
+                    stroke-width="5" fill="none"></path>
+                <!-- 右上 -->
+                <path :d="`M ${image_width - 16} 2.5 L ${image_width - 2.5} 2.5 L ${image_width - 2.5} 16`"
+                    stroke="#1878f5" stroke-width="5" fill="none"></path>
+                <!-- 中左 -->
+                <path :d="`M 2.5 ${(image_height / 2) - 8} L 2.5 ${(image_height / 2) + 8}`" stroke="#1878f5"
+                    stroke-width="5" fill="none"></path>
+                <!-- 中右 -->
+                <path
+                    :d="`M ${image_width - 2.5} ${(image_height / 2) - 8} L ${image_width - 2.5} ${(image_height / 2) + 8}`"
+                    stroke="#1878f5" stroke-width="5" fill="none"></path>
+                <!-- 左下 -->
+                <path :d="`M 2.5 ${image_height - 16} L 2.5 ${image_height - 2.5} L 16 ${image_height - 2.5}`"
+                    stroke="#1878f5" stroke-width="5" fill="none"></path>
+                <!-- 中下 -->
+                <path
+                    :d="`M ${(image_width / 2) - 8} ${image_height - 2.5} L ${(image_width / 2) + 8} ${image_height - 2.5}`"
+                    stroke="#1878f5" stroke-width="5" fill="none"></path>
+                <!-- 右下 -->
+                <path
+                    :d="`M ${image_width - 2.5} ${image_height - 16} L ${image_width - 2.5} ${image_height - 2.5} L ${image_width - 16} ${image_height - 2.5}`"
+                    stroke="#1878f5" stroke-width="5" fill="none"></path>
+            </g>
+            <g v-else>
+                <rect x="2.5" y="2.5" :width="Math.max(image_width - 5, 1)" :height="Math.max(image_height - 5, 1)"
+                    fill="transparent" stroke="#1878f5" stroke-width="5"></rect>
+            </g>
             <!-- 中下 -->
-            <path
-                :d="`M ${(image_width / 2) - 8} ${image_height - 2.5} L ${(image_width / 2) + 8} ${image_height - 2.5}`"
-                stroke="#1878f5" stroke-width="5" fill="none"></path>
+            <rect :x="0" :y="image_height - 6" :width="Math.abs(image_width - 16)" :height="12" fill="transparent"
+                @mousedown="(e) => onMouseDown(e, Direction.Y)"
+                @mouseenter="() => bar_mouseenter(CtrlElementType.RectBottom)" @mouseleave="bar_mouseleave"></rect>
+            <!-- 中右 -->
+            <rect :x="image_width - 6" y="0" :width="12" :height="Math.abs(image_height - 16)" fill="transparent"
+                @mousedown="(e) => onMouseDown(e, Direction.X)"
+                @mouseenter="() => bar_mouseenter(CtrlElementType.RectRight)" @mouseleave="bar_mouseleave"></rect>
             <!-- 右下 -->
-            <path
-                :d="`M ${image_width - 2.5} ${image_height - 16} L ${image_width - 2.5} ${image_height - 2.5} L ${image_width - 16} ${image_height - 2.5}`"
-                stroke="#1878f5" stroke-width="5" fill="none"></path>
+            <rect :x="image_width - 16" :y="image_height - 16" width="22" height="22" fill="transparent"
+                @mousedown="(e) => onMouseDown(e, Direction.Angle)"
+                @mouseenter="() => bar_mouseenter(CtrlElementType.RectRB)" @mouseleave="bar_mouseleave"></rect>
         </g>
-        <g v-else>
-            <rect x="2.5" y="2.5" :width="Math.max(image_width - 5, 1)" :height="Math.max(image_height - 5, 1)"
-                  fill="transparent" stroke="#1878f5" stroke-width="5"></rect>
-        </g>
-        <!-- 中下 -->
-        <rect :x="0" :y="image_height - 6" :width="Math.abs(image_width - 16)" :height="12" fill="transparent"
-              @mousedown="(e) => onMouseDown(e, Direction.Y)"
-              @mouseenter="() => bar_mouseenter(CtrlElementType.RectBottom)"
-              @mouseleave="bar_mouseleave"></rect>
-        <!-- 中右 -->
-        <rect :x="image_width - 6" y="0" :width="12" :height="Math.abs(image_height - 16)" fill="transparent"
-              @mousedown="(e) => onMouseDown(e, Direction.X)"
-              @mouseenter="() => bar_mouseenter(CtrlElementType.RectRight)"
-              @mouseleave="bar_mouseleave"></rect>
-        <!-- 右下 -->
-        <rect :x="image_width - 16" :y="image_height - 16" width="22" height="22" fill="transparent"
-              @mousedown="(e) => onMouseDown(e, Direction.Angle)"
-              @mouseenter="() => bar_mouseenter(CtrlElementType.RectRB)"
-              @mouseleave="bar_mouseleave"></rect>
-    </g>
-</svg>
+    </svg>
 </template>
