9ba4d806539064d0edf204ebb411fbe9e1fb9a95
Zrx <@zrx.com>
2025-03-03 15:49:58 +0800

    fix.pattern`s details

--- src/components/common/ColorBlock/ImagePreview.vue
+++ src/components/common/ColorBlock/ImagePreview.vue
@@ -2,7 +2,7 @@
 
 import { onMounted, onUnmounted, ref, watch } from "vue";
 
-const props = defineProps<{ params: { data: string } }>();
+const props = defineProps<{ params: { data: string }, opacity: number }>();
 const src = ref<string>('');
 
 function draw() {
@@ -13,7 +13,7 @@ onMounted(draw);
 onUnmounted(watch(() => props.params, draw));
 </script>
 <template>
-    <div style="width: 100%; height: 100%; background-color: #fff; position: absolute;">
-        <img :src="src" alt="fill-image" style="width: 100%; height: 100%;object-fit: cover;">
+    <div style="width: 100%; height: 100%; background-color: transparent; position: absolute;">
+        <img :src="src" alt="fill-image" :style="{width: '100%', height: '100%','object-fit': 'cover', opacity}">
     </div>
 </template>
