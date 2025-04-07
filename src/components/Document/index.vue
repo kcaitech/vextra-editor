00a1117bb29c320c9994d4d4587073ddcc532b5b
Zrx <@zrx.com>
2025-04-07 11:46:19 +0800

    fix.组件粘贴异常

--- src/components/Document/index.vue
+++ src/components/Document/index.vue
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 2023-2024 KCai Technology(kcaitech.com). All rights reserved.
- *
- * This file is part of the vextra.io/vextra.cn project, which is licensed under the AGPL-3.0 license.
- * The full license text can be found in the LICENSE file in the root directory of this source tree.
- *
- * For more information about the AGPL-3.0 license, please visit:
- * https://www.gnu.org/licenses/agpl-3.0.html
- */
+* Copyright (c) 2023-2024 KCai Technology(kcaitech.com). All rights reserved.
+*
+* This file is part of the vextra.io/vextra.cn project, which is licensed under the AGPL-3.0 license.
+* The full license text can be found in the LICENSE file in the root directory of this source tree.
+*
+* For more information about the AGPL-3.0 license, please visit:
+* https://www.gnu.org/licenses/agpl-3.0.html
+*/
 
 <script setup lang="ts">
 import { onMounted, onUnmounted, shallowRef, ref, watch } from 'vue';
@@ -179,22 +179,22 @@ onUnmounted(() => {
 </script>
 
 <template>
-<EditorLayout :context="(context as Context)">
-    <template #top>
-        <Toolbar v-if="contentVisible" :context="(context as Context)"/>
-    </template>
-    <template #left>
-        <Navigation v-if="curPage && contentVisible" :context="(context as Context)"
-                    @switchpage="switchPage" :page="(curPage as PageView)"/>
-    </template>
-    <template #center>
-        <ContentView v-if="curPage && !customLoading" :context="(context as Context)" :page="(curPage as PageView)"
-                     @closeLoading="closeLoading" @contentVisible="onContentVisible"/>
-    </template>
-    <template #right>
-        <Attribute v-if="contentVisible" :context="(context as Context)"/>
-    </template>
-</EditorLayout>
-<Bridge v-if="bridge" :context="(context as Context)"/>
-<Loading v-if="loading || customLoading" :size="20"/>
+    <EditorLayout :context="(context as Context)">
+        <template #top>
+            <Toolbar v-if="contentVisible" :context="(context as Context)"/>
+        </template>
+        <template #left>
+            <Navigation v-if="curPage && contentVisible" :context="(context as Context)"
+                        @switchpage="switchPage" :page="(curPage as PageView)"/>
+        </template>
+        <template #center>
+            <ContentView v-if="curPage && !customLoading" :context="(context as Context)" :page="(curPage as PageView)"
+                         @closeLoading="closeLoading" @contentVisible="onContentVisible"/>
+        </template>
+        <template #right>
+            <Attribute v-if="contentVisible" :context="(context as Context)"/>
+        </template>
+    </EditorLayout>
+    <Bridge v-if="bridge" :context="(context as Context)"/>
+    <Loading v-if="loading || customLoading" :size="20"/>
 </template>
