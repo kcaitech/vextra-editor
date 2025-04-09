3f874c5c017efeb3c44de35b63a4ab7b1560bcd2
Zrx <@zrx.com>
2025-04-09 09:12:16 +0800

    fix.keyboard mgr

---
+++ src/keyboard/index.ts
@@ -0,0 +1,26 @@
+import { Context } from "@/context";
+
+interface AddEventListenerOptionsEx extends AddEventListenerOptions {
+    watchInput?: boolean; // 处理输入框内的键盘事件
+}
+
+export class KeyboardMgr {
+    constructor(private context: Context) {
+    }
+
+    addEventListener(type: string, listener: Function, options?: boolean | AddEventListenerOptionsEx | undefined) {
+        const handler = (event: Event) => {
+            // if (!this.context.active) return;
+
+            const target = event.target as HTMLElement;
+
+            if ((target instanceof HTMLInputElement || target instanceof HTMLTextAreaElement) &&
+                (!(typeof options === 'object') || !options?.watchInput)) return;
+
+            listener(event, this.context);
+        };
+
+        document.addEventListener(type, handler, options);
+        return () => document.removeEventListener(type, handler);
+    }
+}
