62f5d485433b4a40e31c338782c50910c2f8d6b9
张瑞强 <zhangruiqiang@kcaitech.com>
2025-05-26 15:26:17 +0800

    更新license head

--- src/components/Document/Navigation/list-icons.ts
+++ src/components/Document/Navigation/list-icons.ts
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2023-2024 KCai Technology(kcaitech.com). All rights reserved.
+ *
+ * This file is part of the Vextra project, which is licensed under the AGPL-3.0 license.
+ * The full license text can be found in the LICENSE file in the root directory of this source tree.
+ *
+ * For more information about the AGPL-3.0 license, please visit:
+ * https://www.gnu.org/licenses/agpl-3.0.html
+ */
+
 // icons.ts
 export const icons: any = {
     layer_image_icon: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="inherit"
