b4504dc2666daa458d9c210624ecfb61e6c45ccd
张瑞强 <zhangruiqiang@kcaitech.com>
2025-06-16 11:33:58 +0800

    整理代码

--- src/index.ts
+++ src/index.ts
@@ -12,122 +12,29 @@
  * 文档编辑器组件
  * @description 用于渲染和编辑设计文档的主要组件
  */
-import _DocumentVue from "./components/Document/index.vue"
+export { default as DocumentVue } from "./components/Document/index.vue"
 
 /**
  * 移动端文档编辑器组件
  * @description 针对移动端优化的文档编辑器组件
  */
-import _MobileDocumentVue from "./components/Mobile/Document.vue"
-import _View from "./components/common/ShapeDesc/View.vue"
+export { default as MobileDocumentVue } from "./components/Mobile/Document.vue"
+
+export { default as View } from "./components/common/ShapeDesc/View.vue"
 
 /**
  * 预览组件
  * @description 用于预览设计文档的组件
  */
-import _PreviewVue from "./components/Preview/index.vue"
-import _StaticShape from "./components/Document/Content/StaticShape.vue"
-import {
-    Coop,
-    creator,
-    DocEditor,
-    Document,
-    IO,
-    TransactDataGuard,
-} from '@kcdesign/data';
+export { default as PreviewVue } from "./components/Preview/index.vue"
 
-import { Context } from "./context";
-import i18n from '@/i18n'
-import { DocumentProps } from "./openapi";
-import { IContext } from "@/openapi";
+export { default as StaticShape } from "./components/Document/Content/StaticShape.vue"
 
 import '@/style/constant.scss'
 import '@/style/app.scss'
-import { initDataModule } from "./basic/initmodule";
 export {i18n_messages as i18n} from '@/i18n';
 
 export * from "./openapi";
-
-export const DocumentVue = _DocumentVue
-
-/**
- * 移动端文档编辑器组件
- * @component
- */
-export const MobileDocumentVue = _MobileDocumentVue
-
-/**
- * 预览组件
- * @component
- */
-export const PreviewVue = _PreviewVue
-
-export const StaticShape = _StaticShape
-
-const t = (i18n as any).global.t;
-
-async function _open(props: DocumentProps) {
-    await initDataModule();
-    const repo = new TransactDataGuard();
-    let cooprepo: Coop.CoopRepository | undefined;
-    let data: Document | undefined;
-    if (props.source === 'storage') {
-        const { document } = await IO.importRemote(props.storage, props.path, props.fid, props.versionId, repo);
-        data = document
-        cooprepo = new Coop.CoopRepository(data, repo)
-    } else if (props.source === 'file') {
-        if (props.fmt === 'sketch') {
-            data = await IO.importSketch(props.file, repo);
-            cooprepo = new Coop.CoopRepository(data, repo)
-        } else if (props.fmt === 'fig') {
-            data = await IO.importFigma(props.file, repo)
-            cooprepo = new Coop.CoopRepository(data, repo)
-        } else if (props.fmt === 'vext' || props.fmt === 'moss') {
-            data = await IO.importVext(props.file, repo);
-            cooprepo = new Coop.CoopRepository(data, repo)
-        }
-    } else if (props.source === 'new') {
-        data = creator.newDocument(t('system.new_file'), repo);
-        cooprepo = new Coop.CoopRepository(data, repo)
-        cooprepo.setInitingDocument(true);
-        const editor = new DocEditor(data, cooprepo);
-        const page = editor.create(t('system.page1'));
-        editor.insert(0, page);
-        cooprepo.setInitingDocument(false);
-    }
-
-    // todo 移动到data
-    if (cooprepo) cooprepo.setBaseVer((data!.lastCmdVer))
-
-    if (data) {
-        return { data, cooprepo: cooprepo! }
-    }
-}
-
-/**
- * 打开文档
- * @param props @see DocumentProps
- * @returns 
- */
-export async function openDocument(props: DocumentProps) {
-    let cooprepo: Coop.CoopRepository | undefined;
-    let data: Document | undefined;
-    try {
-        const result = await _open(props);
-        if (!result) return;
-        cooprepo = result.cooprepo;
-        data = result.data;
-    } catch (e) {
-        console.error(e)
-        return;
-    }
-
-    return new Context(data, cooprepo, props) as IContext;
-}
-
-export async function exportDocument(context: IContext) {
-    return IO.exportVext(context.data, 'blob') as Promise<Blob>;
-}
 
 export { initModule } from "./basic/initmodule";
 
@@ -135,4 +42,4 @@ export { DragKit } from "./components/common/draggable";
 
 export { ShapeDescContext } from "./components/common/ShapeDesc/shapeDescContext";
 
-export const View = _View;
+export { openDocument, exportDocument } from "./functions";
